7ab09caa6222ff198c97b9e11b2c4a065fc69f86
Alexander Chingarev <alexander.chingarev@itextpdf.com>
2014-08-19 14:03:00 +0000

    Added more tests

--- core/src/test/java/com/itextpdf/core/pdf/PdfWriterTest.java
+++ core/src/test/java/com/itextpdf/core/pdf/PdfWriterTest.java
@@ -1,6 +1,8 @@
 package com.itextpdf.core.pdf;
 
 import com.itextpdf.core.exceptions.PdfException;
+import com.itextpdf.text.pdf.PdfReader;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -29,6 +31,97 @@ public class PdfWriterTest {
         PdfPage page = pdfDoc.addNewPage();
         page.flush();
         pdfDoc.close();
+
+        com.itextpdf.text.pdf.PdfReader reader = new PdfReader(destinationFolder + "emptyDocument.pdf");
+        Assert.assertNotNull(reader.getPageN(1));
+        reader.close();
+
+    }
+
+    @Test
+    public void useObjectForMultipleTimes1() throws IOException, PdfException {
+        FileOutputStream fos = new FileOutputStream(destinationFolder + "useObjectForMultipleTimes1.pdf");
+        PdfWriter writer = new PdfWriter(fos);
+        PdfDocument pdfDoc = new PdfDocument(writer);
+
+        PdfDictionary helloWorld = new PdfDictionary(pdfDoc);
+        helloWorld.put(new PdfName("Hello"), new PdfString("World"));
+        PdfPage page = pdfDoc.addNewPage();
+        page.put(new PdfName("HelloWorld"), helloWorld);
+        page.flush();
+        pdfDoc.getCatalog().put(new PdfName("HelloWorld"), helloWorld);
+        pdfDoc.close();
+
+        validateUseObjectForMultipleTimesTest(destinationFolder + "useObjectForMultipleTimes1.pdf");
+    }
+
+    @Test
+    public void useObjectForMultipleTimes2() throws IOException, PdfException {
+        FileOutputStream fos = new FileOutputStream(destinationFolder + "useObjectForMultipleTimes2.pdf");
+        PdfWriter writer = new PdfWriter(fos);
+        PdfDocument pdfDoc = new PdfDocument(writer);
+
+        PdfDictionary helloWorld = new PdfDictionary(pdfDoc);
+        helloWorld.put(new PdfName("Hello"), new PdfString("World"));
+        helloWorld.flush();
+        PdfPage page = pdfDoc.addNewPage();
+        page.put(new PdfName("HelloWorld"), helloWorld);
+        page.flush();
+        pdfDoc.getCatalog().put(new PdfName("HelloWorld"), helloWorld);
+        pdfDoc.close();
+
+        validateUseObjectForMultipleTimesTest(destinationFolder + "useObjectForMultipleTimes2.pdf");
+    }
+
+    @Test
+    public void useObjectForMultipleTimes3() throws IOException, PdfException {
+        FileOutputStream fos = new FileOutputStream(destinationFolder + "useObjectForMultipleTimes3.pdf");
+        PdfWriter writer = new PdfWriter(fos);
+        PdfDocument pdfDoc = new PdfDocument(writer);
+
+        PdfDictionary helloWorld = new PdfDictionary(pdfDoc);
+        helloWorld.put(new PdfName("Hello"), new PdfString("World"));
+        PdfPage page = pdfDoc.addNewPage();
+        page.put(new PdfName("HelloWorld"), helloWorld);
+        page.flush();
+        helloWorld.flush();
+        pdfDoc.getCatalog().put(new PdfName("HelloWorld"), helloWorld);
+        pdfDoc.close();
+
+        validateUseObjectForMultipleTimesTest(destinationFolder + "useObjectForMultipleTimes3.pdf");
+    }
+
+    @Test
+    public void useObjectForMultipleTimes4() throws IOException, PdfException {
+        FileOutputStream fos = new FileOutputStream(destinationFolder + "useObjectForMultipleTimes4.pdf");
+        PdfWriter writer = new PdfWriter(fos);
+        PdfDocument pdfDoc = new PdfDocument(writer);
+
+        PdfDictionary helloWorld = new PdfDictionary(pdfDoc);
+        helloWorld.put(new PdfName("Hello"), new PdfString("World"));
+        PdfPage page = pdfDoc.addNewPage();
+        page.put(new PdfName("HelloWorld"), helloWorld);
+        page.flush();
+        pdfDoc.getCatalog().put(new PdfName("HelloWorld"), helloWorld);
+        helloWorld.flush();
+        pdfDoc.close();
+
+        validateUseObjectForMultipleTimesTest(destinationFolder + "useObjectForMultipleTimes4.pdf");
+    }
+
+    private void validateUseObjectForMultipleTimesTest(String filename) throws IOException {
+        com.itextpdf.text.pdf.PdfReader reader = new PdfReader(filename);
+        com.itextpdf.text.pdf.PdfDictionary page = reader.getPageN(1);
+        Assert.assertNotNull(page);
+        com.itextpdf.text.pdf.PdfDictionary helloWorld = page.getAsDict(new com.itextpdf.text.pdf.PdfName("HelloWorld"));
+        Assert.assertNotNull(helloWorld);
+        com.itextpdf.text.pdf.PdfString world = helloWorld.getAsString(new com.itextpdf.text.pdf.PdfName("Hello"));
+        Assert.assertEquals("World", world.toString());
+        helloWorld = reader.getCatalog().getAsDict(new com.itextpdf.text.pdf.PdfName("HelloWorld"));
+        Assert.assertNotNull(helloWorld);
+        world = helloWorld.getAsString(new com.itextpdf.text.pdf.PdfName("Hello"));
+        Assert.assertEquals("World", world.toString());
+        reader.close();
     }
 
 }
