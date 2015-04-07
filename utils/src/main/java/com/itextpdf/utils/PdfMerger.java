1de435ef49ed2481d411666973b77088ff0911e6
pavel.morozov <pavel.morozov@duallab.com>
2015-04-07 17:21:49 +0300

    Added comments for PdfMerger class

--- utils/src/main/java/com/itextpdf/utils/PdfMerger.java
+++ utils/src/main/java/com/itextpdf/utils/PdfMerger.java
@@ -13,10 +13,21 @@ public class PdfMerger {
     private PdfDocument pdfDocument;
     private ArrayList<PdfPage> pagesToCopy = new ArrayList<PdfPage>();
 
+    /**
+     * This class is used to merge a number of existing documents into one;
+     * @param pdfDocument - the document into which source documents will be merged.
+     */
     public PdfMerger(PdfDocument pdfDocument){
         this.pdfDocument = pdfDocument;
     }
 
+    /**
+     * This method adds pages from the source document to the List of pages which will be merged.
+     * @param from - document, from which pages will be copied.
+     * @param fromPage - start page in the range of pages to be copied.
+     * @param toPage - end page in the range to be copied.
+     * @throws PdfException
+     */
     public void addPages(PdfDocument from, int fromPage, int toPage) throws PdfException {
         LinkedHashMap<PdfPage, PdfPage> page2page = new LinkedHashMap<PdfPage, PdfPage>();
         for (int pageNum = fromPage; pageNum <= toPage; pageNum++){
@@ -25,6 +36,12 @@ public class PdfMerger {
         createStructTreeRoot(from, page2page);
     }
 
+    /**
+     * This method adds pages from the source document to the List of pages which will be merged.
+     * @param from - document, from which pages will be copied.
+     * @param pages - List of numbers of pages which will be copied.
+     * @throws PdfException
+     */
     public void addPages(PdfDocument from, ArrayList<Integer> pages) throws PdfException {
         LinkedHashMap<PdfPage, PdfPage> page2page = new LinkedHashMap<PdfPage, PdfPage>();
         for (Integer pageNum : pages){
@@ -33,12 +50,23 @@ public class PdfMerger {
         createStructTreeRoot(from, page2page);
     }
 
+    /**
+     * This method gets all pages from the List of pages to be copied and merges them into one document.
+     * @throws PdfException
+     */
     public void merge() throws PdfException {
         for (PdfPage page : pagesToCopy){
             pdfDocument.addPage(page);
         }
     }
 
+    /**
+     * This method fills the List of pages to be copied with given page.
+     * @param from - document, from which pages will be copied.
+     * @param pageNum - number of page to be copied.
+     * @param page2page - map, which contains original page as a key and new page of the new document as a value. This map is used to create StructTreeRoot in the new document.
+     * @throws PdfException
+     */
     private void fillListOfPagesToCopy(PdfDocument from, int pageNum, LinkedHashMap<PdfPage, PdfPage> page2page) throws PdfException {
         PdfPage originalPage = from.getPage(pageNum);
         PdfPage newPage = originalPage.copy(pdfDocument);
@@ -46,6 +74,12 @@ public class PdfMerger {
         pagesToCopy.add(newPage);
     }
 
+    /**
+     * This method creates StructTreeRoot in the new document.
+     * @param from - document, from which pages will be copied.
+     * @param page2page - map, which contains original page as a key and new page of the new document as a value. This map is used to create StructTreeRoot in the new document.
+     * @throws PdfException
+     */
     private void createStructTreeRoot(PdfDocument from, LinkedHashMap<PdfPage, PdfPage> page2page) throws PdfException {
         PdfStructTreeRoot structTreeRoot = from.getStructTreeRoot();
         if (structTreeRoot != null)
