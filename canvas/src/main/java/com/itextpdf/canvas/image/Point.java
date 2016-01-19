d494d028119e0010c763ee70e6b9a2de041a2a5a
Michael Demey <michael.demey@itextpdf.com>
2016-01-19 10:45:37 +0100

    Document Canvas: Point

--- canvas/src/main/java/com/itextpdf/canvas/image/Point.java
+++ canvas/src/main/java/com/itextpdf/canvas/image/Point.java
@@ -1,13 +1,33 @@
 package com.itextpdf.canvas.image;
 
+/**
+ * Represents a 2-dimensional point.
+ */
 public class Point {
 
+    /**
+     * The X value of the point.
+     */
     public int x;
+
+    /**
+     * The Y value of the point.
+     */
     public int y;
 
+    /**
+     * Creates a point without coordinates.
+     */
     public Point() {
+        // empty body
     }
 
+    /**
+     * Creates a point and sets the two values as its coordinates.
+     *
+     * @param x the X value of the point
+     * @param y the Y value of the point
+     */
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
