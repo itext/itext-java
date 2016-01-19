0d9054034a039397146204409de3b49499199c88
Michael Demey <michael.demey@itextpdf.com>
2016-01-19 11:41:55 +0100

    Document Canvas: InputMeta & MetaObject

--- canvas/src/main/java/com/itextpdf/canvas/image/InputMeta.java
+++ canvas/src/main/java/com/itextpdf/canvas/image/InputMeta.java
@@ -7,15 +7,29 @@ import com.itextpdf.core.color.DeviceRgb;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Helper class to read nt, short, words, etc. from an InputStream.
+ */
 public class InputMeta {
 
     InputStream in;
     int length;
 
+    /**
+     * Creates an InputMeta object.
+     *
+     * @param in InputStream containing the WMF data
+     */
     public InputMeta(InputStream in) {
         this.in = in;
     }
 
+    /**
+     * Read the next word from the InputStream.
+     *
+     * @return the next word or 0 if the end of the stream has been reached
+     * @throws IOException
+     */
     public int readWord() throws IOException {
         length += 2;
         int k1 = in.read();
@@ -24,6 +38,12 @@ public class InputMeta {
         return (k1 + (in.read() << 8)) & 0xffff;
     }
 
+    /**
+     * Read the next short from the InputStream.
+     *
+     * @return the next short value
+     * @throws IOException
+     */
     public int readShort() throws IOException{
         int k = readWord();
         if (k > 0x7fff)
@@ -31,6 +51,12 @@ public class InputMeta {
         return k;
     }
 
+    /**
+     * Read the next int from the InputStream.
+     *
+     * @return the next int
+     * @throws IOException
+     */
     public int readInt() throws IOException{
         length += 4;
         int k1 = in.read();
@@ -41,20 +67,43 @@ public class InputMeta {
         return k1 + k2 + k3 + (in.read() << 24);
     }
 
+    /**
+     * Read the next byte from the InputStream.
+     *
+     * @return the next byte
+     * @throws IOException
+     */
     public int readByte() throws IOException{
         ++length;
         return in.read() & 0xff;
     }
 
+    /**
+     * Skips "len" amount of bytes from the InputStream. If len is < 0, nothing is skipped.
+     *
+     * @param len amount of bytes needed to skip
+     * @throws IOException
+     */
     public void skip(int len) throws IOException{
         length += len;
         Utilities.skip(in, len);
     }
 
+    /**
+     * Get the amount of bytes read and/or skipped from the InputStream.
+     *
+     * @return number of bytes read
+     */
     public int getLength() {
         return length;
     }
 
+    /**
+     * Read the next {@link com.itextpdf.core.color.Color} from the InputStream. This reads 4 bytes.
+     *
+     * @return the next Color
+     * @throws IOException
+     */
     public Color readColor() throws IOException{
         int red = readByte();
         int green = readByte();
--- canvas/src/main/java/com/itextpdf/canvas/image/MetaObject.java
+++ canvas/src/main/java/com/itextpdf/canvas/image/MetaObject.java
@@ -1,5 +1,8 @@
 package com.itextpdf.canvas.image;
 
+/**
+ * A meta object.
+ */
 public class MetaObject {
 
     public static final int META_NOT_SUPPORTED = 0;
@@ -8,13 +11,27 @@ public class MetaObject {
     public static final int META_FONT = 3;
     public int type = META_NOT_SUPPORTED;
 
+    /**
+     * Creates a new MetaObject. This constructor doesn't set the type.
+     */
     public MetaObject() {
+        // Empty body
     }
 
+    /**
+     * Creates a MetaObject with a type.
+     *
+     * @param type the type of meta object
+     */
     public MetaObject(int type) {
         this.type = type;
     }
-    
+
+    /**
+     * Get the type of this MetaObject.
+     *
+     * @return type of MetaObject
+     */
     public int getType() {
         return type;
     }
