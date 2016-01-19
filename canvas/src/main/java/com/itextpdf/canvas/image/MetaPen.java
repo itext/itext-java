756df62286ac9dd73f884bbe389e76ab379b2589
Michael Demey <michael.demey@itextpdf.com>
2016-01-19 12:01:40 +0100

    Document Canvas: MetaPen

--- canvas/src/main/java/com/itextpdf/canvas/image/MetaPen.java
+++ canvas/src/main/java/com/itextpdf/canvas/image/MetaPen.java
@@ -5,6 +5,9 @@ import com.itextpdf.core.color.DeviceRgb;
 
 import java.io.IOException;
 
+/**
+ * A Pen object of the WMF format. Holds the color, style and width information of the pen.
+ */
 public class MetaPen extends MetaObject {
 
     public static final int PS_SOLID = 0;
@@ -19,25 +22,49 @@ public class MetaPen extends MetaObject {
     int penWidth = 1;
     Color color = DeviceRgb.BLACK;
 
+    /**
+     * Creates a MetaPen object.
+     */
     public MetaPen() {
         type = META_PEN;
     }
 
+    /**
+     * Initializes a MetaPen object.
+     *
+     * @param in the InputMeta object that holds the inputstream of the WMF image
+     * @throws IOException
+     */
     public void init(InputMeta in) throws IOException {
         style = in.readWord();
         penWidth = in.readShort();
         in.readWord();
         color = in.readColor();
     }
-    
+
+    /**
+     * Get the style of the MetaPen.
+     *
+     * @return style of the pen
+     */
     public int getStyle() {
         return style;
     }
-    
+
+    /**
+     * Get the width of the MetaPen.
+     *
+     * @return width of the pen
+     */
     public int getPenWidth() {
         return penWidth;
     }
-    
+
+    /**
+     * Get the color of the MetaPen.
+     *
+     * @return color of the pen
+     */
     public Color getColor() {
         return color;
     }
