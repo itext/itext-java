03e0caffd2ec05d0b6fd13e2f481408456b66c40
Michael Demey <michael.demey@itextpdf.com>
2016-01-20 10:27:33 +0100

    Document Core.security: ExternalDecryptionProcess

--- core/src/main/java/com/itextpdf/core/security/ExternalDecryptionProcess.java
+++ core/src/main/java/com/itextpdf/core/security/ExternalDecryptionProcess.java
@@ -3,11 +3,23 @@ package com.itextpdf.core.security;
 import org.bouncycastle.cms.Recipient;
 import org.bouncycastle.cms.RecipientId;
 
+/**
+ * Interface to externalize the retrieval of the CMS recipient info.
+ */
 public interface ExternalDecryptionProcess {
 
+    /**
+     * Returns the ID of the CMS recipient.
+     *
+     * @return ID of the CMS Recipient
+     */
     RecipientId getCmsRecipientId();
 
+    /**
+     * Returns the CMS recipient
+     *
+     * @return CMS Recipient
+     */
     Recipient getCmsRecipient();
 
 }
-
