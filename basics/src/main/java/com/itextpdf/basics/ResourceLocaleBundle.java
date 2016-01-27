ed3efb361297f8f03367874357eda259b26f3ec3
Dmitry Trusevich <dmitry.trusevich@duallab.com>
2016-01-27 18:04:24 +0300

    fix locale comparison

--- basics/src/main/java/com/itextpdf/basics/ResourceLocaleBundle.java
+++ basics/src/main/java/com/itextpdf/basics/ResourceLocaleBundle.java
@@ -13,13 +13,13 @@ import java.util.Set;
 public class ResourceLocaleBundle {
 
     public final static Locale DEFAULT_LOCALE = Locale.ENGLISH;
-    public final static Set<Locale> locales = new HashSet(Arrays.asList(Locale.ENGLISH, new Locale("pt_PT"), new Locale("nl_NL")));
+    public final static Set<String> locales = new HashSet(Arrays.asList("en", "pt", "nl"));
     private final static String RESOURCE_BUNDLE_ERROR_MESSAGE = "Exception message {0} is not found in the file";
 
 
     public synchronized static String getMessage(String path, String key, List<Object> args) {
         Locale currentLocale = Locale.getDefault();
-        if (!locales.equals(locales)) {
+        if (!locales.contains(currentLocale.getLanguage())) {
             currentLocale = DEFAULT_LOCALE;
         }
         ResourceBundle bundle = ResourceBundle.getBundle(path, currentLocale);
