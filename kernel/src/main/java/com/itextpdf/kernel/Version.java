2cd5e10fbf2ee20838af7a62c9f3d7ac4889bab3
Pavel Alay <pavel.alay@duallab.com>
2016-01-29 19:15:09 +0300

    Update version and name.

--- kernel/src/main/java/com/itextpdf/kernel/Version.java
+++ kernel/src/main/java/com/itextpdf/kernel/Version.java
@@ -20,14 +20,14 @@ public final class Version {
      * This String contains the version number of this iText release.
      * For debugging purposes, we request you NOT to change this constant.
      */
-    private String release = "6.0.0-SNAPSHOT";
+    private String release = "7.0.0-SNAPSHOT";
     /**
      * This String contains the iText version as shown in the producer line.
      * iText is a product developed by iText Group NV.
      * iText Group requests that you retain the iText producer line
      * in every PDF that is created or manipulated using iText.
      */
-    private String iTextVersion = iText + " " + release + " \u00a92000-2014 iText Group NV (AGPL-version)";
+    private String iTextVersion = iText + " " + release + " \u00a92000-2016 iText Group NV (AGPL-version)";
     /**
      * The license key.
      */
