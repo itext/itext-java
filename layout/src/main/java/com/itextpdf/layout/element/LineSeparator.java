f4a3b15c18634d98855f8eb929bbf180c52bc8b9
Alexey Subach <alexey.subach@duallab.com>
2016-02-16 21:44:21 +0300

    Add JavaDoc to LineSeparator class

--- layout/src/main/java/com/itextpdf/layout/element/LineSeparator.java
+++ layout/src/main/java/com/itextpdf/layout/element/LineSeparator.java
@@ -7,11 +7,20 @@ import com.itextpdf.layout.Property;
 import com.itextpdf.layout.renderer.BlockRenderer;
 import com.itextpdf.layout.renderer.LineSeparatorRenderer;
 
+/**
+ * This is a line separator element which is basically just a horizontal line with
+ * a style specified by {@link LineDrawer} custom drawing interface instance.
+ * This might be thought of as an HTML's <hr> element alternative.
+ */
 public class LineSeparator extends BlockElement<LineSeparator> {
 
     protected PdfName role = PdfName.Artifact;
     protected AccessibleElementProperties tagProperties;
 
+    /**
+     * Creates a custom line separator with line style defined by custom {@link LineDrawer} interface instance
+     * @param lineDrawer line drawer instance
+     */
     public LineSeparator(LineDrawer lineDrawer) {
         setProperty(Property.LINE_DRAWER, lineDrawer);
     }
