ce0bdd033a3ff0286998d3d63119b38192a42427
Alexey Subach <alexey.subach@duallab.com>
2016-02-16 11:53:52 +0300

    Review sandbox.objects samples
    DEVSIX-423

--- kernel/src/main/java/com/itextpdf/kernel/pdf/canvas/draw/SolidLine.java
+++ kernel/src/main/java/com/itextpdf/kernel/pdf/canvas/draw/SolidLine.java
@@ -9,14 +9,47 @@ import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
  */
 public class SolidLine implements Drawable {
 
+    private float lineWidth = .5f;
+
+    /**
+     * Constructs an instance of solid line drawer
+     */
+    public SolidLine() {
+    }
+
+    /**
+     * Constructs an instance of solid line drawer with the specified line thickness
+     * @param lineWidth line width
+     */
+    public SolidLine(float lineWidth) {
+        this.lineWidth = lineWidth;
+    }
+
     @Override
     public void draw(PdfCanvas canvas, Rectangle drawArea) {
         canvas.saveState();
-        canvas.setLineWidth(0.5f);
+        canvas.setLineWidth(lineWidth);
         canvas
                 .moveTo(drawArea.getX(), drawArea.getY())
                 .lineTo(drawArea.getX() + drawArea.getWidth(), drawArea.getY())
                 .stroke()
                 .restoreState();
     }
+
+    /**
+     * Gets line width in points
+     * @return line thickness
+     */
+    public float getLineWidth() {
+        return lineWidth;
+    }
+
+    /**
+     * Sets line width in points
+     * @param lineWidth new line width
+     */
+    public void setLineWidth(float lineWidth) {
+        this.lineWidth = lineWidth;
+    }
+
 }
