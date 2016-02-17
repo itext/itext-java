b45a249de78d58ef1187c8f4d0ea72b417e4a15d
Benoit Lagae <benoit.lagae@itextpdf.com>
2016-02-17 16:51:07 +0100

    Document layout.element.Text

--- layout/src/main/java/com/itextpdf/layout/element/Text.java
+++ layout/src/main/java/com/itextpdf/layout/element/Text.java
@@ -6,12 +6,20 @@ import com.itextpdf.kernel.pdf.tagutils.IAccessibleElement;
 import com.itextpdf.layout.Property;
 import com.itextpdf.layout.renderer.TextRenderer;
 
+/**
+ * A {@link Text} is a piece of text of any length. As a {@link ILeafElement leaf element},
+ * it is the smallest piece of content that may bear specific layout attributes.
+ */
 public class Text extends AbstractElement<Text> implements ILeafElement<Text>, IElement<Text>, IAccessibleElement {
 
     protected String text;
     protected PdfName role = PdfName.Span;
     protected AccessibleElementProperties tagProperties;
 
+    /**
+     * Constructs a Text with its role initialized.
+     * @param text the contents, as a {@link String}
+     */
     public Text(String text) {
         this.text = text;
     }
@@ -35,14 +43,28 @@ public class Text extends AbstractElement<Text> implements ILeafElement<Text>, I
         this.text = text;
     }
 
+    /**
+     * Gets the text rise.
+     * @return the vertical distance from the text's default base line, as a float.
+     */
     public float getTextRise() {
         return getProperty(Property.TEXT_RISE);
     }
 
+    /**
+     * Sets the text rise.
+     * @param textRise a vertical distance from the text's default base line.
+     * @return this Text
+     */
     public Text setTextRise(float textRise) {
         return setProperty(Property.TEXT_RISE, textRise);
     }
 
+    /**
+     * Gets the horizontal scaling property, which determines how wide the text
+     * should be stretched.
+     * @return the horizontal spacing, as a <code>float</code>
+     */
     public Float getHorizontalScaling() {
         return getProperty(Property.HORIZONTAL_SCALING);
     }
@@ -68,6 +90,7 @@ public class Text extends AbstractElement<Text> implements ILeafElement<Text>, I
      * @param horizontalScaling the scaling parameter. 1 means no scaling will be applied,
      *                          0.5 means the text will be scaled by half.
      *                          2 means the text will be twice as wide as normal one.
+     * @return this Text
      */
     public Text setHorizontalScaling(float horizontalScaling) {
         return setProperty(Property.HORIZONTAL_SCALING, horizontalScaling);
