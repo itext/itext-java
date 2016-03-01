67058f056470633a9761caffc92f1420b8918684
pavel.morozov <pavel.morozov@duallab.com>
2016-03-01 16:27:57 +0300

    Implement 3D annotations
    DEVSIX-477

--- kernel/src/main/java/com/itextpdf/kernel/pdf/annot/Pdf3DAnnotation.java
+++ kernel/src/main/java/com/itextpdf/kernel/pdf/annot/Pdf3DAnnotation.java
@@ -23,15 +23,31 @@ public class Pdf3DAnnotation extends PdfAnnotation {
         return put(PdfName._3DV, initialView);
     }
 
+    public PdfObject getDefaultInitialView() {
+        return getPdfObject().get(PdfName._3DV);
+    }
+
     public Pdf3DAnnotation setActivationDictionary(PdfDictionary activationDictionary) {
         return put(PdfName._3DA, activationDictionary);
+    }
+
+    public PdfDictionary getActivationDictionary() {
+        return getPdfObject().getAsDictionary(PdfName._3DA);
     }
 
     public Pdf3DAnnotation setInteractive(boolean interactive) {
         return put(PdfName._3DI, new PdfBoolean(interactive));
     }
 
+    public PdfBoolean isInteractive() {
+        return getPdfObject().getAsBoolean(PdfName._3DI);
+    }
+
     public Pdf3DAnnotation setViewBox(Rectangle viewBox) {
         return put(PdfName._3DB, new PdfArray(viewBox));
+    }
+
+    public Rectangle getViewBox() {
+        return getPdfObject().getAsRectangle(PdfName._3DB);
     }
 }
