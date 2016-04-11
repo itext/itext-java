7777709d04920da3210e9d88325422c4c9a7f28b
Amedee Van Gasse <amedee.vangasse@itextpdf.com>
2016-04-11 17:07:46 +0200

    Change package comment files from HTML to Java (preferred from JDK 5.0)

---
+++ kernel/src/main/java/com/itextpdf/kernel/xmp/impl/xpath/package-info.java
@@ -0,0 +1,5 @@
+/**
+    <p>Package containing the XMPPath handling.</p>
+    <p>An XMPPath a simplified form of an XPath, used only to create or retrieve properties in an XMPMeta object.<p>
+ */
+package com.itextpdf.kernel.xmp.impl.xpath;
