42c863d849dc3743a2a48c3670faef2173972926
Alexey Subach <alexey.subach@duallab.com>
2016-06-28 11:39:24 +0300

    Fix failing tests after commit 2209042d2b82 (LineWidth was ignored for SolidLine; added @Override for DashedLine)
    DEVSIX-703

--- layout/src/test/java/com/itextpdf/layout/TabsTest.java
+++ layout/src/test/java/com/itextpdf/layout/TabsTest.java
@@ -16,15 +16,16 @@ import com.itextpdf.layout.element.TabStop;
 import com.itextpdf.layout.property.TabAlignment;
 import com.itextpdf.test.ExtendedITextTest;
 import com.itextpdf.test.annotations.type.IntegrationTest;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 @Category(IntegrationTest.class)
 public class TabsTest extends ExtendedITextTest {
