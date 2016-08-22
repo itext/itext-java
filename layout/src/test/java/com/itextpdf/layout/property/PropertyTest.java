460553e5a5d90a174e7a93f62aa09bac6676cb23
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2016-08-22 14:48:14 +0300

    Use MessageFormat.format instead of String.format for the sake of porting to .net

--- layout/src/test/java/com/itextpdf/layout/property/PropertyTest.java
+++ layout/src/test/java/com/itextpdf/layout/property/PropertyTest.java
@@ -24,7 +24,7 @@ public class PropertyTest extends ExtendedITextTest {
                 int value = (int) field.get(null);
                 maxFieldValue = Math.max(maxFieldValue, value);
                 if (fieldValues.contains(value)) {
-                    Assert.fail(String.format("Multiple fields with same value: %s", value));
+                    Assert.fail(MessageFormat.format("Multiple fields with same value: {0}", value));
                 }
                 fieldValues.add(value);
             }
