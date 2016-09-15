20acc1acb4bee4a01c6c0e28004c0b7aeba3558e
Pavel Alay <pavel.alay@duallab.com>
2016-09-15 19:58:12 +0300

    Update naming strategy for FormFieldFlatteningTest

--- forms/src/test/java/com/itextpdf/forms/FormFieldFlatteningTest.java
+++ forms/src/test/java/com/itextpdf/forms/FormFieldFlatteningTest.java
@@ -46,17 +46,17 @@ public class FormFieldFlatteningTest extends ExtendedITextTest {
 
     @Test
     public void formFlatteningTest_DefaultAppearanceGeneration_Rot0() throws IOException, InterruptedException {
-        String srcFilePattern = "FormFlatteningDefaultAppearance_0.0_";
-        String destPattern = "FormFlatteningDefaultAppearance_0.0_";
+        String srcFilePattern = "FormFlatteningDefaultAppearance_0_";
+        String destPattern = "FormFlatteningDefaultAppearance_0_";
 
-        for(float i = 0; i<360;i+=90){
-            String src = sourceFolder + srcFilePattern + i+".pdf";
-            String dest = destinationFolder + destPattern + i +"_flattened.pdf";
+        for (int i = 0; i < 360; i += 90) {
+            String src = sourceFolder + srcFilePattern + i + ".pdf";
+            String dest = destinationFolder + destPattern + i + "_flattened.pdf";
             String cmp = sourceFolder + "cmp_" + srcFilePattern + i + ".pdf";
             PdfDocument doc = new PdfDocument(new PdfReader(src), new PdfWriter(dest));
 
             PdfAcroForm form = PdfAcroForm.getAcroForm(doc, true);
-            for(PdfFormField field: form.getFormFields().values()){
+            for (PdfFormField field : form.getFormFields().values()) {
                 field.setValue("Test");
             }
             form.flattenFields();
@@ -74,17 +74,17 @@ public class FormFieldFlatteningTest extends ExtendedITextTest {
 
     @Test
     public void formFlatteningTest_DefaultAppearanceGeneration_Rot90() throws IOException, InterruptedException {
-        String srcFilePattern = "FormFlatteningDefaultAppearance_90.0_";
-        String destPattern = "FormFlatteningDefaultAppearance_90.0_";
+        String srcFilePattern = "FormFlatteningDefaultAppearance_90_";
+        String destPattern = "FormFlatteningDefaultAppearance_90_";
 
-        for(float i = 0; i<360;i+=90){
-            String src = sourceFolder + srcFilePattern + i+".pdf";
-            String dest = destinationFolder + destPattern + i +"_flattened.pdf";
+        for (int i = 0; i < 360; i += 90) {
+            String src = sourceFolder + srcFilePattern + i + ".pdf";
+            String dest = destinationFolder + destPattern + i + "_flattened.pdf";
             String cmp = sourceFolder + "cmp_" + srcFilePattern + i + ".pdf";
             PdfDocument doc = new PdfDocument(new PdfReader(src), new PdfWriter(dest));
 
             PdfAcroForm form = PdfAcroForm.getAcroForm(doc, true);
-            for(PdfFormField field: form.getFormFields().values()){
+            for (PdfFormField field : form.getFormFields().values()) {
                 field.setValue("Test");
             }
             form.flattenFields();
@@ -102,17 +102,17 @@ public class FormFieldFlatteningTest extends ExtendedITextTest {
 
     @Test
     public void formFlatteningTest_DefaultAppearanceGeneration_Rot180() throws IOException, InterruptedException {
-        String srcFilePattern = "FormFlatteningDefaultAppearance_180.0_";
-        String destPattern = "FormFlatteningDefaultAppearance_180.0_";
+        String srcFilePattern = "FormFlatteningDefaultAppearance_180_";
+        String destPattern = "FormFlatteningDefaultAppearance_180_";
 
-        for(float i = 0; i<360;i+=90){
-            String src = sourceFolder + srcFilePattern + i+".pdf";
-            String dest = destinationFolder + destPattern + i +"_flattened.pdf";
+        for (int i = 0; i < 360; i += 90) {
+            String src = sourceFolder + srcFilePattern + i + ".pdf";
+            String dest = destinationFolder + destPattern + i + "_flattened.pdf";
             String cmp = sourceFolder + "cmp_" + srcFilePattern + i + ".pdf";
             PdfDocument doc = new PdfDocument(new PdfReader(src), new PdfWriter(dest));
 
             PdfAcroForm form = PdfAcroForm.getAcroForm(doc, true);
-            for(PdfFormField field: form.getFormFields().values()){
+            for (PdfFormField field : form.getFormFields().values()) {
                 field.setValue("Test");
             }
             form.flattenFields();
@@ -130,17 +130,17 @@ public class FormFieldFlatteningTest extends ExtendedITextTest {
 
     @Test
     public void formFlatteningTest_DefaultAppearanceGeneration_Rot270() throws IOException, InterruptedException {
-        String srcFilePattern = "FormFlatteningDefaultAppearance_270.0_";
-        String destPattern = "FormFlatteningDefaultAppearance_270.0_";
+        String srcFilePattern = "FormFlatteningDefaultAppearance_270_";
+        String destPattern = "FormFlatteningDefaultAppearance_270_";
 
-        for(float i = 0; i<360;i+=90){
-            String src = sourceFolder + srcFilePattern + i+".pdf";
-            String dest = destinationFolder + destPattern + i +"_flattened.pdf";
+        for (int i = 0; i < 360; i += 90) {
+            String src = sourceFolder + srcFilePattern + i + ".pdf";
+            String dest = destinationFolder + destPattern + i + "_flattened.pdf";
             String cmp = sourceFolder + "cmp_" + srcFilePattern + i + ".pdf";
             PdfDocument doc = new PdfDocument(new PdfReader(src), new PdfWriter(dest));
 
             PdfAcroForm form = PdfAcroForm.getAcroForm(doc, true);
-            for(PdfFormField field: form.getFormFields().values()){
+            for (PdfFormField field : form.getFormFields().values()) {
                 field.setValue("Test");
             }
             form.flattenFields();
