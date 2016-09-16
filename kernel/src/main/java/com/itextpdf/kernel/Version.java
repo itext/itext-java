bc8b13d07712460a63dc010f2d322690ef181bd4
Amedee Van Gasse <amedee.vangasse@itextpdf.com>
2016-09-16 13:49:52 +0200

    [RELEASE] 7.0.1-SNAPSHOT -> 7.0.2-SNAPSHOT

--- kernel/src/main/java/com/itextpdf/kernel/Version.java
+++ kernel/src/main/java/com/itextpdf/kernel/Version.java
@@ -72,7 +72,7 @@ public final class Version {
      * This String contains the version number of this iText release.
      * For debugging purposes, we request you NOT to change this constant.
      */
-    private static String release = "7.0.1-SNAPSHOT";
+    private static String release = "7.0.2-SNAPSHOT";
     /**
      * This String contains the iText version as shown in the producer line.
      * iText is a product developed by iText Group NV.
