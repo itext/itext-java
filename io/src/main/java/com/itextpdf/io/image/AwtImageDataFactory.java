649b644e9248a5b419aa7a672d55b19d8dbfc629
Alexey Subach <alexey.subach@duallab.com>
2016-09-22 10:06:40 +0300

    Fix Image binary transparency bug (fix by Thomas Bungener)
    Transparent pixels in ARGB images sometimes were mistreated as non transparent. See http://stackoverflow.com/questions/39119776/itext-binary-transparency-bug
    
    This commit also contains additional fix suggested by Michael Klink
    
    DEV-1796

--- io/src/main/java/com/itextpdf/io/image/AwtImageDataFactory.java
+++ io/src/main/java/com/itextpdf/io/image/AwtImageDataFactory.java
@@ -189,8 +189,25 @@ class AwtImageDataFactory {
                                 transparency[0] = transparency[1] = transparentPixel >> 16 & 0xff;
                                 transparency[2] = transparency[3] = transparentPixel >> 8 & 0xff;
                                 transparency[4] = transparency[5] = transparentPixel & 0xff;
+                                // Added by Michael Klink
+                                // Check whether this value for transparent pixels
+                                // has already been used for a non-transparent one
+                                // before this position
+                                for (int prevPixel = 0; prevPixel < j; prevPixel++) {
+                                    if ((pixels[prevPixel] & 0xffffff) == transparentPixel) {
+                                        // found a prior use of the transparentPixel color
+                                        // and, therefore, cannot make use of this color
+                                        // for transparency; we could still use an image
+                                        // mask but for simplicity let's use a soft mask
+                                        // which already is implemented here
+                                        shades = true;
+                                        break;
+                                    }
+                                }
                             }
-                        } else if ((pixels[j] & 0xffffff) != transparentPixel) {
+                        } else if (((pixels[j] & 0xffffff) != transparentPixel) && (alpha == 0)) {
+                            shades = true;
+                        } else if (((pixels[j] & 0xffffff) == transparentPixel) && (alpha != 0)) {
                             shades = true;
                         }
                     }
