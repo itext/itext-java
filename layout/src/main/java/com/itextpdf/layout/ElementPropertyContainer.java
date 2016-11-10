469ac82f5f29ab843f25491ea49cf48f98a1c3ae
Alexey Subach <alexey.subach@duallab.com>
2016-11-10 10:18:39 +0300

    Add missing import

--- layout/src/main/java/com/itextpdf/layout/ElementPropertyContainer.java
+++ layout/src/main/java/com/itextpdf/layout/ElementPropertyContainer.java
@@ -59,7 +59,7 @@ import com.itextpdf.layout.property.Underline;
 import com.itextpdf.layout.property.UnitValue;
 import com.itextpdf.layout.splitting.ISplitCharacters;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
