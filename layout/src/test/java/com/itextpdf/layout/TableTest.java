c394c7b83462fd3223be5763846bf9281c0c73f5
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2016-11-28 09:30:38 +0300

    Add task reference to ignored ticket

--- layout/src/test/java/com/itextpdf/layout/TableTest.java
+++ layout/src/test/java/com/itextpdf/layout/TableTest.java
@@ -992,7 +992,7 @@ public class TableTest extends ExtendedITextTest {
         Assert.assertNull(new CompareTool().compareByContent(outFileName, cmpFileName, destinationFolder, testName + "_diff"));
     }
 
-    @Ignore
+    @Ignore("DEVISX-929")
     @LogMessages(messages = {
             @LogMessage(messageTemplate = LogMessageConstant.ELEMENT_DOES_NOT_FIT_AREA, count = 9)
     })
