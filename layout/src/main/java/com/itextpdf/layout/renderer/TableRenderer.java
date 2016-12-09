9ecf4150602bd86aaea804a578f27761e4b99b4b
Pavel Morozov <pavel.morozov@duallab.com>
2016-12-09 14:54:23 +0300

    ArrayIndexOutOfBoundException int html2pdf during processing nested tables with rowspan
    DEVSIX-972

--- layout/src/main/java/com/itextpdf/layout/renderer/TableRenderer.java
+++ layout/src/main/java/com/itextpdf/layout/renderer/TableRenderer.java
@@ -1393,6 +1393,11 @@ public class TableRenderer extends AbstractRenderer {
     }
 
     private void buildBordersArrays(CellRenderer cell, int row, int col) {
+        //We should check if the row number is less than horizontal borders array size. It can happen if the cell with
+        //big rowspan doesn't fit current area and is going to be placed partial.
+        if (row > horizontalBorders.size()) {
+            row = horizontalBorders.size() - 1;
+        }
         if (cell != null) {
             buildBordersArrays(cell, row, true, false);
         }
