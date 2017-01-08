3b797a410f4101c5f6a7fe3fba80485df9c69c9c
Richard Cohn <rcohn@adobe.com>
2017-01-08 09:26:25 -0500

    Reverse order of Style lookup in AbstractElement.getProperty.
    Look in styles for a property in the reverse order the Styles are added. This enables a client to
    add a new Style to an Element that can override the effect of previously added Styles. Otherwise
    there is no way to change the property using a Style without changing the underlying Style.  The
    lookup order doesn't seem to be documented anywhere, so while this is an incompatible change, it
    doesn't seem to affect the documentation.
    The existing tests of addStyle in StylesTest are unaffected, but the new test would have failed with
    the old code.

--- layout/src/main/java/com/itextpdf/layout/element/AbstractElement.java
+++ layout/src/main/java/com/itextpdf/layout/element/AbstractElement.java
@@ -52,9 +52,9 @@ import com.itextpdf.layout.property.Property;
 import com.itextpdf.layout.renderer.IRenderer;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Defines the most common properties that most {@link IElement} implementations
@@ -66,7 +66,7 @@ public abstract class AbstractElement<T extends IElement> extends ElementPropert
 
     protected IRenderer nextRenderer;
     protected List<IElement> childElements = new ArrayList<>();
-    protected Set<Style> styles;
+    protected LinkedList<Style> styles;
 
     @Override
     public IRenderer getRenderer() {
@@ -110,7 +110,9 @@ public abstract class AbstractElement<T extends IElement> extends ElementPropert
     public <T1> T1 getProperty(int property) {
         Object result = super.<T1>getProperty(property);
         if (styles != null && styles.size() > 0 && result == null && !super.hasProperty(property)) {
-            for (Style style : styles) {
+            Iterator<Style> listItReverse = styles.descendingIterator();
+            while(listItReverse.hasNext()) {
+                Style style = listItReverse.next();
                 result = style.<T1>getProperty(property);
                 if (result != null || super.hasProperty(property)) {
                     break;
@@ -128,7 +130,7 @@ public abstract class AbstractElement<T extends IElement> extends ElementPropert
      */
     public T addStyle(Style style) {
         if (styles == null) {
-            styles = new LinkedHashSet<>();
+            styles = new LinkedList<>();
         }
         styles.add(style);
         return (T) (Object)this;
