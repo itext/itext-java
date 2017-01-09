7bcf85c2a3503f9c6c532cba3c2b57a12739e9ff
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2017-01-09 20:23:06 +0300

    Minor refactoring

--- layout/src/main/java/com/itextpdf/layout/font/FontProgramInfo.java
+++ layout/src/main/java/com/itextpdf/layout/font/FontProgramInfo.java
@@ -16,7 +16,7 @@
     or FITNESS FOR A PARTICULAR PURPOSE.
     See the GNU Affero General Public License for more details.
     You should have received a copy of the GNU Affero General Public License
-    along with this program; if no  t, see http://www.gnu.org/licenses or write to
+    along with this program; if not, see http://www.gnu.org/licenses or write to
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA, 02110-1301 USA, or download the license from the following URL:
     http://itextpdf.com/terms-of-use/
