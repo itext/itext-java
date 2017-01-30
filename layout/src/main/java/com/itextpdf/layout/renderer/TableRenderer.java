itext/itext-java

commit befff0724b0f2e7e4158cc7fb9c43ee32f4bf78e

Fix bug with min-max width calculations when all cells in column are null.
DEVSIX-138

layout/src/main/java/com/itextpdf/layout/renderer/TableRenderer.java
@@ -1530,6 +1530,9 @@ public class TableRenderer extends AbstractRenderer {
                     curColspan = cellsColspan[row][col];
                     maxColumnsWidth[col + 1] = Math.max(maxColumnsWidth[col + 1], cellsMinMaxWidth[row][col].getMaxWidth() + maxColumnsWidth[col - curColspan + 1]);
                     minColumnsWidth[col + 1] = Math.max(minColumnsWidth[col + 1], cellsMinMaxWidth[row][col].getMinWidth() + minColumnsWidth[col - curColspan + 1]);
+                } else {
+                    maxColumnsWidth[col + 1] = maxColumnsWidth[col];
+                    minColumnsWidth[col + 1] = minColumnsWidth[col];
                 }
             }
         }

commit af0b628c2acba21b433271b14657f03ffad0fe5e

Fix bug introduced by commit [befff0724b]
DEVSIX-138

layout/src/main/java/com/itextpdf/layout/renderer/TableRenderer.java
@@ -1531,8 +1531,8 @@ public class TableRenderer extends AbstractRenderer {
                     maxColumnsWidth[col + 1] = Math.max(maxColumnsWidth[col + 1], cellsMinMaxWidth[row][col].getMaxWidth() + maxColumnsWidth[col - curColspan + 1]);
                     minColumnsWidth[col + 1] = Math.max(minColumnsWidth[col + 1], cellsMinMaxWidth[row][col].getMinWidth() + minColumnsWidth[col - curColspan + 1]);
                 } else {
-                    maxColumnsWidth[col + 1] = maxColumnsWidth[col];
-                    minColumnsWidth[col + 1] = minColumnsWidth[col];
+                    maxColumnsWidth[col + 1] = Math.max(maxColumnsWidth[col + 1], maxColumnsWidth[col]);
+                    minColumnsWidth[col + 1] = Math.max(minColumnsWidth[col + 1], minColumnsWidth[col]);
                 }
             }
         }