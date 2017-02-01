2f7e243c1ef80625275f970cf26a65224e2f07b0
Alexey Subach <alexey.subach@duallab.com>
2017-02-01 17:38:32 +0300

    WebColor.getRGBAColor returns null if it cannot parse a color

--- kernel/src/main/java/com/itextpdf/kernel/color/WebColors.java
+++ kernel/src/main/java/com/itextpdf/kernel/color/WebColors.java
@@ -44,11 +44,11 @@
 package com.itextpdf.kernel.color;
 
 import com.itextpdf.io.LogMessageConstant;
-import java.text.MessageFormat;
-import java.util.HashMap;
-import java.util.StringTokenizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 /**
  * This class is a HashMap that contains the names of colors as a key and the
@@ -218,17 +218,21 @@ public class WebColors extends HashMap<String, int[]> {
      */
     public static DeviceRgb getRGBColor(String name) {
         float[] rgbaColor = getRGBAColor(name);
-        return new DeviceRgb(rgbaColor[0], rgbaColor[1], rgbaColor[2]);
+        if (rgbaColor == null) {
+            return new DeviceRgb(0, 0, 0);
+        } else {
+            return new DeviceRgb(rgbaColor[0], rgbaColor[1], rgbaColor[2]);
+        }
     }
 
     /**
      * Gives an array of four floats that contain RGBA values, each value is between 0 and 1. 
      * @param name a name such as black, violet, cornflowerblue or #RGB or
      *             #RRGGBB or RGB or RRGGBB or rgb(R,G,B) or rgb(R,G,B,A)
-     * @return the corresponding array of four floats.
+     * @return the corresponding array of four floats, or <code>null</code> if parsing failed.
      */
     public static float[] getRGBAColor(String name) {
-        float[] color = {0, 0, 0, 1};
+        float[] color = null;
         try {
             String colorName = name.toLowerCase();
             boolean colorStrWithoutHash = missingHashColorFormat(colorName);
@@ -239,12 +243,14 @@ public class WebColors extends HashMap<String, int[]> {
                 }
                 if (colorName.length() == 3) {
                     String red = colorName.substring(0, 1);
+                    color = new float[]{0, 0, 0, 1};
                     color[0] = (float) (Integer.parseInt(red + red, 16) / RGB_MAX_VAL);
                     String green = colorName.substring(1, 2);
                     color[1] = (float) (Integer.parseInt(green + green, 16) / RGB_MAX_VAL);
                     String blue = colorName.substring(2);
                     color[2] = (float) (Integer.parseInt(blue + blue, 16) / RGB_MAX_VAL);
                 } else if (colorName.length() == 6) {
+                    color = new float[]{0, 0, 0, 1};
                     color[0] = (float) (Integer.parseInt(colorName.substring(0, 2), 16) / RGB_MAX_VAL);
                     color[1] = (float) (Integer.parseInt(colorName.substring(2, 4), 16) / RGB_MAX_VAL);
                     color[2] = (float) (Integer.parseInt(colorName.substring(4), 16) / RGB_MAX_VAL);
@@ -255,28 +261,26 @@ public class WebColors extends HashMap<String, int[]> {
             } else if (colorName.startsWith("rgb(")) {
                 final String delim = "rgb(), \t\r\n\f";
                 StringTokenizer tok = new StringTokenizer(colorName, delim);
+                color = new float[]{0, 0, 0, 1};
                 parseRGBColors(color, tok);
             } else if (colorName.startsWith("rgba(")) {
                 final String delim = "rgba(), \t\r\n\f";
                 StringTokenizer tok = new StringTokenizer(colorName, delim);
+                color = new float[]{0, 0, 0, 1};
                 parseRGBColors(color, tok);
                 if (tok.hasMoreTokens()) {
                     color[3] = getAlphaChannelValue(tok.nextToken());
                 }
-
-            } else if (!NAMES.containsKey(colorName)) {
-                Logger logger = LoggerFactory.getLogger(WebColors.class);
-                logger.error(MessageFormat.format(LogMessageConstant.COLOR_NOT_FOUND, colorName));
-            } else {
+            } else if (NAMES.containsKey(colorName)) {
                 int[] intColor = NAMES.get(colorName);
+                color = new float[]{0, 0, 0, 1};
                 color[0] = (float) (intColor[0] / RGB_MAX_VAL);
                 color[1] = (float) (intColor[1] / RGB_MAX_VAL);
                 color[2] = (float) (intColor[2] / RGB_MAX_VAL);
             }
-        } catch (Exception e) {
-            Logger logger = LoggerFactory.getLogger(WebColors.class);
-            logger.error(MessageFormat.format(LogMessageConstant.COLOR_NOT_PARSED, name));
-            color = new float[]{0, 0, 0, 1};
+        } catch (Exception exc) {
+            // Will just return null in this case
+            color = null;
         }
 
         return color;
