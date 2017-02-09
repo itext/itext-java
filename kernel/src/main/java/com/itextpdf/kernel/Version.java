b44704459405d186ec7e7651dcb82ef4f74f124f
Amedee Van Gasse <amedee.vangasse@itextpdf.com>
2017-02-09 14:33:45 +0100

    Update year in Version.java
    DEVSIX-1028

--- kernel/src/main/java/com/itextpdf/kernel/Version.java
+++ kernel/src/main/java/com/itextpdf/kernel/Version.java
@@ -79,7 +79,7 @@ public final class Version {
      * iText Group requests that you retain the iText producer line
      * in every PDF that is created or manipulated using iText.
      */
-    private String iTextVersion = iText + " " + release + " \u00a92000-2016 iText Group NV";
+    private String iTextVersion = iText + " " + release + " \u00a92000-2017 iText Group NV";
 
     /**
      * The license key.
