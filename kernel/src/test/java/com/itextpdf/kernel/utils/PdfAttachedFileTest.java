c5044c290d1df2bae16595cceb97847e3d067d69
joris.schellekens <joris.schellekens@itextpdf.com>
2017-04-19 14:01:35 +0200

    Build convenience API for accessing attachments of pdf documents
    DEVSIX-763

---
+++ kernel/src/test/java/com/itextpdf/kernel/utils/PdfAttachedFileTest.java
@@ -0,0 +1,92 @@
+package com.itextpdf.kernel.utils;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.test.annotations.type.UnitTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Created by Joris Schellekens on 5/2/2017.
+ */
+@Category(UnitTest.class)
+public class PdfAttachedFileTest {
+
+    public static final String sourceFolder = "./src/test/resources/com/itextpdf/kernel/utils/PdfAttachedFileTest/";
+    public static final String destinationFolder = "./target/test/com/itextpdf/kernel/utils/PdfAttachedFileTest/";
+
+    @Test
+    public void extractSingleAttachmentFromPdf()
+    {
+        String filename = sourceFolder + "FileWithSingleAttachment.pdf";
+        String attachmentFilename = sourceFolder + "FileWithSingleAttachment_001.jpg";
+
+        try {
+            PdfDocument pdfDocument = new PdfDocument(new PdfReader(filename));
+
+            // create
+            PdfAttachedFile rootFile = new PdfAttachedFile(pdfDocument);
+
+            // assert number of attachments
+            assert(rootFile.listFiles().length == 1);
+
+            // check content of attachments
+            PdfAttachedFile attachment = (PdfAttachedFile) rootFile.listFiles()[0];
+            byte[] bytes0 = attachment.getBytes();
+            byte[] bytes1 = Files.readAllBytes(new File(attachmentFilename).toPath());
+
+            assert(bytes0.length == bytes1.length);
+            for(int i=0;i<bytes0.length;i++)
+                assert(bytes0[i] == bytes1[i]);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+    @Test
+    public void extractMultipleAttachmentFromPdf()
+    {
+        String filename = sourceFolder + "FileWithMultipleAttachments.pdf";
+        String[] attachmentFilename = { sourceFolder + "FileWithMultipleAttachments_001.jpg",
+                                        sourceFolder + "FileWithMultipleAttachments_002.jpg",
+                                        sourceFolder + "FileWithMultipleAttachments_003.jpg",
+                                        sourceFolder + "FileWithMultipleAttachments_004.jpg"
+                                            };
+
+        try {
+            PdfDocument pdfDocument = new PdfDocument(new PdfReader(filename));
+
+            // create
+            PdfAttachedFile rootFile = new PdfAttachedFile(pdfDocument);
+
+            // assert number of attachments
+            assert(rootFile.listFiles().length == attachmentFilename.length);
+
+            // check content of attachments
+            for(int i=0;i<rootFile.listFiles().length;i++) {
+
+                PdfAttachedFile attachment = (PdfAttachedFile) rootFile.listFiles()[i];
+                byte[] bytes0 = attachment.getBytes();
+                byte[] bytes1 = Files.readAllBytes(new File(attachmentFilename[i]).toPath());
+
+                // compare bytes
+                assert (bytes0.length == bytes1.length);
+                for (int j = 0; j < bytes0.length; j++)
+                    assert (bytes0[j] == bytes1[j]);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            assert(false);
+        }
+    }
+
+
+}
