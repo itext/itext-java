4625ce5201e3110892370bd7440d3bc248c71ce6
Pavel Alay <pavel.alay@duallab.com>
2017-05-10 13:50:30 +0300

    SmartModePdfObjectsSerializer: fix serialization issues
    DEVSIX-729

--- kernel/src/main/java/com/itextpdf/kernel/pdf/SmartModePdfObjectsSerializer.java
+++ kernel/src/main/java/com/itextpdf/kernel/pdf/SmartModePdfObjectsSerializer.java
@@ -1,13 +1,17 @@
 package com.itextpdf.kernel.pdf;
 
 import com.itextpdf.kernel.PdfException;
+
+import java.io.Serializable;
 import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.Map;
 
-class SmartModePdfObjectsSerializer {
+class SmartModePdfObjectsSerializer implements Serializable {
 
-    private MessageDigest md5;
+    private static final long serialVersionUID = 2502203520776244051L;
+
+    private transient MessageDigest md5;
     private HashMap<SerializedObjectContent, PdfIndirectReference> serializedContentToObj = new HashMap<>();
 
     SmartModePdfObjectsSerializer() {
