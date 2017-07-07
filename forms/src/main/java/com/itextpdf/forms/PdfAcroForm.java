30135114afcdbee5e6061a4ffb5c8b948091d1dc
Benoit Lagae <benoit.lagae@itextpdf.com>
2017-07-07 20:09:59 +0200

    Use form name: getValue() instead of toString()
    DEVSIX-1357

--- forms/src/main/java/com/itextpdf/forms/PdfAcroForm.java
+++ forms/src/main/java/com/itextpdf/forms/PdfAcroForm.java
@@ -845,10 +845,10 @@ public class PdfAcroForm extends PdfObjectWrapper<PdfDictionary> {
                         parentField = PdfFormField.makeFormField(parentField.getParent(), document);
                     }
                 }
-                name = fieldName.toString() + "." + index;
+                name = fieldName.getValue() + "." + index;
                 index++;
             } else {
-                name = fieldName.toString();
+                name = fieldName.getValue();
             }
             fields.put(name, formField);
             if (formField.getKids() != null) {
