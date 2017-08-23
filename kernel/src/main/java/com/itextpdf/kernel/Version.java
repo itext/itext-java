754bf861bfa44a28c2957c8c7b36ed53857dfc99
Valera Goman <valera.goman@duallab.com>
2017-08-23 13:54:59 +0300

    Update pom-s
    7.0.5-SNAPSHOT -> 7.1.0-SNAPSHOT
    
    DEVSIX-1308

--- kernel/src/main/java/com/itextpdf/kernel/Version.java
+++ kernel/src/main/java/com/itextpdf/kernel/Version.java
@@ -72,7 +72,7 @@ public final class Version {
      * This String contains the version number of this iText release.
      * For debugging purposes, we request you NOT to change this constant.
      */
-    private static String release = "7.0.5-SNAPSHOT";
+    private static String release = "7.1.0-SNAPSHOT";
     /**
      * This String contains the iText version as shown in the producer line.
      * iText is a product developed by iText Group NV.
