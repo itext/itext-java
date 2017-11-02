09f4447fb557f9e1be51a37fcfe1ed587cb59c43
Pavel Alay <pavel.alay@duallab.com>
2017-11-02 13:53:03 +0300

    Revert default font names in FontConstants
    Those constants are too popular, let's keep it.
    
    DEVSIX-1013

--- io/src/main/java/com/itextpdf/io/font/FontConstants.java
+++ io/src/main/java/com/itextpdf/io/font/FontConstants.java
@@ -43,20 +43,154 @@
  */
 package com.itextpdf.io.font;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Font constants for {@link FontProgramFactory} and PdfFontFactory.
+ * @deprecated Use constants from com.itextpdf.io.font.constants.
  */
+@Deprecated
 public class FontConstants {
 
     //-Font styles------------------------------------------------------------------------------------------------------
+    /**
+     * Undefined font style.
+     *
+     * @deprecated use {@link com.itextpdf.io.font.constants.FontStyles#UNDEFINED} instead.
+     */
+    @Deprecated
+    public static final int UNDEFINED = -1;
+
+    /**
+     * Normal font style.
+     * @deprecated use {@link com.itextpdf.io.font.constants.FontStyles#NORMAL} instead.
+     */
+    @Deprecated
+    public static final int NORMAL = 0;
+
+    /**
+     * Bold font style.
+     * @deprecated use {@link com.itextpdf.io.font.constants.FontStyles#BOLD} instead.
+     */
+    @Deprecated
+    public static final int BOLD = 1;
+
+    /**
+     * Italic font style.
+     * @deprecated use {@link com.itextpdf.io.font.constants.FontStyles#ITALIC} instead.
+     */
+    @Deprecated
+    public static final int ITALIC = 2;
+    /**
+     * Bold-Italic font style.
+     * @deprecated use {@link com.itextpdf.io.font.constants.FontStyles#BOLDITALIC} instead.
+     */
+    @Deprecated
+    public static final int BOLDITALIC = BOLD | ITALIC;
 
 
     //-Default fonts----------------------------------------------------------------------------------------------------
-    //TODO return and deprecate!
 
-    //-Font Descriptor--------------------------------------------------------------------------------------------------
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#COURIER} instead.
+     */
+    @Deprecated
+    public static final String COURIER = "Courier";
 
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#COURIER_BOLD} instead.
+     */
+    @Deprecated
+    public static final String COURIER_BOLD = "Courier-Bold";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#COURIER_OBLIQUE} instead.
+     */
+    @Deprecated
+    public static final String COURIER_OBLIQUE = "Courier-Oblique";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#COURIER_BOLDOBLIQUE} instead.
+     */
+    @Deprecated
+    public static final String COURIER_BOLDOBLIQUE = "Courier-BoldOblique";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#HELVETICA} instead.
+     */
+    @Deprecated
+    public static final String HELVETICA = "Helvetica";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#HELVETICA_BOLD} instead.
+     */
+    @Deprecated
+    public static final String HELVETICA_BOLD = "Helvetica-Bold";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#HELVETICA_OBLIQUE} instead.
+     */
+    @Deprecated
+    public static final String HELVETICA_OBLIQUE = "Helvetica-Oblique";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#HELVETICA_BOLDOBLIQUE} instead.
+     */
+    @Deprecated
+    public static final String HELVETICA_BOLDOBLIQUE = "Helvetica-BoldOblique";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#SYMBOL} instead.
+     */
+    @Deprecated
+    public static final String SYMBOL = "Symbol";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#TIMES_ROMAN} instead.
+     */
+    @Deprecated
+    public static final String TIMES_ROMAN = "Times-Roman";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#TIMES_BOLD} instead.
+     */
+    @Deprecated
+    public static final String TIMES_BOLD = "Times-Bold";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#TIMES_ITALIC} instead.
+     */
+    @Deprecated
+    public static final String TIMES_ITALIC = "Times-Italic";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#TIMES_BOLDITALIC} instead.
+     */
+    @Deprecated
+    public static final String TIMES_BOLDITALIC = "Times-BoldItalic";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#ZAPFDINGBATS} instead.
+     */
+    @Deprecated
+    public static final String ZAPFDINGBATS = "ZapfDingbats";
+
+    /**
+     * This is a possible value of a base 14 type 1 font
+     * @deprecated use {@link com.itextpdf.io.font.constants.StandardFonts#TIMES} instead.
+     */
+    @Deprecated
+    public static final String TIMES = "Times";
 }
