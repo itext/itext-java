7a78398aac2d51746d4b3eeff1ad1c9809397563
Amedee Van Gasse <amedee.vangasse@itextpdf.com>
2017-11-23 15:34:14 +0100

    Restore Unicode characters in Utf8FormsTest.java
    RND-486

--- forms/src/test/java/com/itextpdf/forms/Utf8FormsTest.java
+++ forms/src/test/java/com/itextpdf/forms/Utf8FormsTest.java
@@ -1,45 +1,3 @@
-/*
-    This file is part of the iText (R) project.
-    Copyright (c) 1998-2017 iText Group NV
-    Authors: iText Software.
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License version 3
-    as published by the Free Software Foundation with the addition of the
-    following permission added to Section 15 as permitted in Section 7(a):
-    FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
-    ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
-    OF THIRD PARTY RIGHTS
-
-    This program is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-    or FITNESS FOR A PARTICULAR PURPOSE.
-    See the GNU Affero General Public License for more details.
-    You should have received a copy of the GNU Affero General Public License
-    along with this program; if not, see http://www.gnu.org/licenses or write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA, 02110-1301 USA, or download the license from the following URL:
-    http://itextpdf.com/terms-of-use/
-
-    The interactive user interfaces in modified source and object code versions
-    of this program must display Appropriate Legal Notices, as required under
-    Section 5 of the GNU Affero General Public License.
-
-    In accordance with Section 7(b) of the GNU Affero General Public License,
-    a covered work must retain the producer line in every PDF that is created
-    or manipulated using iText.
-
-    You can be released from the requirements of the license by purchasing
-    a commercial license. Buying such a license is mandatory as soon as you
-    develop commercial activities involving the iText software without
-    disclosing the source code of your own applications.
-    These activities include: offering paid services to customers as an ASP,
-    serving PDFs on the fly in a web application, shipping iText with a closed
-    source product.
-
-    For more information, please contact iText Software Corp. at this
-    address: sales@itextpdf.com
- */
 package com.itextpdf.forms;
 
 import com.itextpdf.forms.fields.PdfFormField;
@@ -83,7 +41,7 @@ public class Utf8FormsTest {
         Map<String,PdfFormField> fields = form.getFormFields();
         pdfDoc.close();
         for (String fldName : fields.keySet()) {
-            //  ??
+            //  لا
             Assert.assertEquals("\u0644\u0627", fldName);
         }
         pdfDoc.close();
@@ -97,7 +55,7 @@ public class Utf8FormsTest {
         Map<String,PdfFormField> fields = form.getFormFields();
         pdfDoc.close();
         for (String fldName : fields.keySet()) {
-            //  ?? ??UTF8
+            //  福昕 福昕UTF8
             Assert.assertEquals("\u798F\u6615 \u798F\u6615UTF8", fields.get(fldName).getValueAsString());
         }
     }
@@ -109,9 +67,9 @@ public class Utf8FormsTest {
         PdfTextFormField field = PdfTextFormField.createText(pdfDoc,
                 new Rectangle(99, 753, 425, 15), "", "");
         field.setFont(PdfFontFactory.createFont(FONT, PdfEncodings.IDENTITY_H));
-        //  ??
+        //  لا
         field.put(PdfName.T, new PdfString("\u0644\u0627", PdfEncodings.UTF8));
-        //  ?? ??UTF8
+        //  福昕 福昕UTF8
         field.put(PdfName.V, new PdfString("\u798F\u6615 \u798F\u6615UTF8", PdfEncodings.UTF8));
         field.regenerateField();
         form.addField(field);
