c4f937a46b6932270d19caaba3ed395b08d4f9e8
Ilya Idamkin <ilya.idamkin@duallab.com>
2017-12-04 14:36:07 +0300

    Add new date parse util method

--- io/src/main/java/com/itextpdf/io/util/DateTimeUtil.java
+++ io/src/main/java/com/itextpdf/io/util/DateTimeUtil.java
@@ -43,6 +43,8 @@
  */
 package com.itextpdf.io.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -78,5 +80,13 @@ public final class DateTimeUtil {
         cal.setTime(date);
         cal.add(Calendar.DAY_OF_YEAR, days);
         return cal.getTime();
+    }
+
+    public static Date parseSimpleFormat(String date, String format) {
+        try {
+            return new SimpleDateFormat(format).parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
