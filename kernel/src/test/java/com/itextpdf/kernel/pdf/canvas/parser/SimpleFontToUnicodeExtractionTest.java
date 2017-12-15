06f3273d3ab80e5ba748640d809accbde33c87cb
Alexey Subach <alexey.subach@duallab.com>
2017-12-15 17:36:30 +0300

    Add category to a test

--- kernel/src/test/java/com/itextpdf/kernel/pdf/canvas/parser/SimpleFontToUnicodeExtractionTest.java
+++ kernel/src/test/java/com/itextpdf/kernel/pdf/canvas/parser/SimpleFontToUnicodeExtractionTest.java
@@ -4,11 +4,14 @@ import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.canvas.parser.listener.LocationTextExtractionStrategy;
 import com.itextpdf.test.ExtendedITextTest;
+import com.itextpdf.test.annotations.type.IntegrationTest;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import java.io.IOException;
 
+@Category(IntegrationTest.class)
 public class SimpleFontToUnicodeExtractionTest extends ExtendedITextTest {
 
     private static final String sourceFolder = "./src/test/resources/com/itextpdf/kernel/parser/SimpleFontToUnicodeExtractionTest/";
