07f9561dd2e4d710acc6b4ccb2b090801bf1381d
mrsafi <msafiyullah@gmail.com>
2018-01-09 15:11:33 +0530

    fixed findFieldName method not  giving the result  more than one time.

--- forms/src/main/java/com/itextpdf/forms/xfa/XfaForm.java
+++ forms/src/main/java/com/itextpdf/forms/xfa/XfaForm.java
@@ -55,17 +55,11 @@ import com.itextpdf.kernel.pdf.PdfString;
 import com.itextpdf.kernel.pdf.PdfVersion;
 import com.itextpdf.kernel.pdf.VersionConforming;
 import com.itextpdf.kernel.xmp.XmlDomWriter;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.EntityResolver;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -76,6 +70,14 @@ import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 /**
  * Processes XFA forms.
@@ -331,10 +333,14 @@ public class XfaForm {
      * @return the complete name or <CODE>null</CODE> if not found
      */
     public String findFieldName(String name) {
-        if (acroFieldsSom == null && xfaPresent) {
+        if (acroFieldsSom == null && xfaPresent && datasetsSom != null ) {
             acroFieldsSom = new AcroFieldsSearch(datasetsSom.getName2Node().keySet());
+        }
+
+        if (acroFieldsSom != null && xfaPresent) {
             return acroFieldsSom.getAcroShort2LongName().containsKey(name) ? acroFieldsSom.getAcroShort2LongName().get(name) : acroFieldsSom.inverseSearchGlobal(Xml2Som.splitParts(name));
         }
+
         return null;
     }
 
