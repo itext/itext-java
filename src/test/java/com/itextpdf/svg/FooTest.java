aeeb4c925dc8c4b3f3dcd3e8c0e4b2ad1f84c5d2
Michael Demey <michael.demey@itextpdf.com>
2018-03-07 15:15:14 +0100

    removed hamcrest usage RND-746

--- src/test/java/com/itextpdf/svg/FooTest.java
+++ src/test/java/com/itextpdf/svg/FooTest.java
@@ -1,16 +1,14 @@
 package com.itextpdf.svg;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
 
 public class FooTest {
 
     @Test
     public void test() {
         Foo foo = new Foo();
-        assertThat(foo.toString(), equalTo("Hello, world"));
+        Assert.assertEquals(foo.toString(), "Hello, world");
     }
 
 }
