25f63242236d807db0f455023b50996a2531b7d6
Amedee Van Gasse <amedee.vangasse@itextpdf.com>
2018-03-08 16:53:39 +0100

    Add javadoc

--- src/main/java/com/itextpdf/svg/Foo.java
+++ src/main/java/com/itextpdf/svg/Foo.java
@@ -1,7 +1,13 @@
 package com.itextpdf.svg;
 
+/**
+ * @author Amedee Van Gasse
+ */
 public class Foo {
 
+    /**
+     * Returns a common phrase for code examples
+     */
     public String toString() {
         return "Hello, world";
     }
--- src/test/java/com/itextpdf/svg/FooTest.java
+++ src/test/java/com/itextpdf/svg/FooTest.java
@@ -1,14 +1,15 @@
 package com.itextpdf.svg;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FooTest {
 
     @Test
     public void test() {
         Foo foo = new Foo();
-        Assert.assertEquals(foo.toString(), "Hello, world");
+        assertEquals(foo.toString(), "Hello, world");
     }
 
 }
