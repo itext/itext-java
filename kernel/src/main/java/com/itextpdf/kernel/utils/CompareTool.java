20b9a05067490bf27c8c20ba38e4f9afa87a6d28
Ilya Idamkin <ilya.idamkin@duallab.com>
2018-03-29 12:30:45 +0300

    Compare producer line on document info comparison
    Note that pdfXFA and iTextSharp producer line is treated the same way
    as regular iText one.
    
    DEVSIX-1870

--- kernel/src/main/java/com/itextpdf/kernel/utils/CompareTool.java
+++ kernel/src/main/java/com/itextpdf/kernel/utils/CompareTool.java
@@ -131,6 +131,10 @@ public class CompareTool {
     private static final String gsParams = " -dNOPAUSE -dBATCH -sDEVICE=png16m -r150 -sOutputFile='<outputfile>' '<inputfile>'";
     private static final String compareParams = " '<image1>' '<image2>' '<difference>'";
 
+    private static final String versionRegexp = "(iText\u00ae( pdfX(FA|fa))?|iTextSharp\u2122) (\\d\\.)+\\d(-SNAPSHOT)?";
+    private static final String versionReplacement = "iText\u00ae <version>";
+    private static final String copyrightRegexp = "\u00a9\\d+-\\d+ iText Group NV";
+    private static final String copyrightReplacement = "\u00a9<copyright years> iText Group NV";
 
     private String gsExec;
     private String compareExec;
@@ -650,7 +654,7 @@ public class CompareTool {
         String[] outInfo = convertInfo(outDocument.getDocumentInfo());
         for (int i = 0; i < cmpInfo.length; ++i) {
             if (!cmpInfo[i].equals(outInfo[i])){
-                message = "Document info fail";
+                message = MessageFormatUtil.format("Document info fail. Expected: \"{0}\", actual: \"{1}\"", cmpInfo[i], outInfo[i]);
                 break;
             }
         }
@@ -1614,7 +1618,7 @@ public class CompareTool {
     }
 
     private String[] convertInfo(PdfDocumentInfo info) {
-        String[] convertedInfo = new String[]{"", "", "", ""};
+        String[] convertedInfo = new String[]{"", "", "", "", ""};
         String infoValue = info.getTitle();
         if (infoValue != null)
             convertedInfo[0] = infoValue;
@@ -1627,9 +1631,16 @@ public class CompareTool {
         infoValue = info.getKeywords();
         if (infoValue != null)
             convertedInfo[3] = infoValue;
+        infoValue = info.getProducer();
+        if (infoValue != null) {
+            convertedInfo[4] = convertProducerLine(infoValue);
+        }
         return convertedInfo;
     }
 
+    private String convertProducerLine(String producer) {
+        return producer.replaceAll(versionRegexp, versionReplacement).replaceAll(copyrightRegexp, copyrightReplacement);
+    }
 
     private class PngFileFilter implements FileFilter {
 
