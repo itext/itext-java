1e50abbb5ae8f4117bc557b020606d953b35371b
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2018-07-19 16:52:07 +0300

    Merge CssUtilsTest and CssUtilTest.
    DEVSIX-1998
    DEVSIX-2053

--- src/test/java/com/itextpdf/styledxmlparser/css/util/CssUtilTest.java
+++ src/test/java/com/itextpdf/styledxmlparser/css/util/CssUtilTest.java
@@ -42,17 +42,92 @@
  */
 package com.itextpdf.styledxmlparser.css.util;
 
+import com.itextpdf.io.util.MessageFormatUtil;
+import com.itextpdf.styledxmlparser.LogMessageConstant;
 import com.itextpdf.styledxmlparser.css.CommonCssConstants;
+import com.itextpdf.styledxmlparser.exceptions.StyledXMLParserException;
 import com.itextpdf.test.ExtendedITextTest;
+import com.itextpdf.test.annotations.LogMessage;
+import com.itextpdf.test.annotations.LogMessages;
 import com.itextpdf.test.annotations.type.UnitTest;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 @Category(UnitTest.class)
 public class CssUtilTest extends ExtendedITextTest {
+
+    @Rule
+    public ExpectedException junitExpectedException = ExpectedException.none();
+
+    @Test
+    public void parseAbsoluteLengthFromNAN() {
+        junitExpectedException.expect(StyledXMLParserException.class);
+        junitExpectedException.expectMessage(MessageFormatUtil.format(LogMessageConstant.NAN, "Definitely not a number"));
+
+        String value = "Definitely not a number";
+        CssUtils.parseAbsoluteLength(value);
+    }
+
+    @Test
+    public void parseAbsoluteLengthFromNull() {
+        junitExpectedException.expect(StyledXMLParserException.class);
+        junitExpectedException.expectMessage(MessageFormatUtil.format(LogMessageConstant.NAN, "null"));
+
+        String value = null;
+        CssUtils.parseAbsoluteLength(value);
+    }
+
+    @Test
+    public void parseAbsoluteLengthFrom10px() {
+        String value = "10px";
+        float actual = CssUtils.parseAbsoluteLength(value, CommonCssConstants.PX);
+        float expected = 7.5f;
+
+        Assert.assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void parseAbsoluteLengthFrom10cm() {
+        String value = "10cm";
+        float actual = CssUtils.parseAbsoluteLength(value, CommonCssConstants.CM);
+        float expected = 283.46457f;
+
+        Assert.assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void parseAbsoluteLengthFrom10in() {
+        String value = "10in";
+        float actual = CssUtils.parseAbsoluteLength(value, CommonCssConstants.IN);
+        float expected = 720.0f;
+
+        Assert.assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void parseAbsoluteLengthFrom10pt() {
+        String value = "10pt";
+        float actual = CssUtils.parseAbsoluteLength(value, CommonCssConstants.PT);
+        float expected = 10.0f;
+
+        Assert.assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    @LogMessages(messages = {@LogMessage(messageTemplate = LogMessageConstant.UNKNOWN_ABSOLUTE_METRIC_LENGTH_PARSED, count = 1)})
+    public void parseAbsoluteLengthFromUnknownType() {
+        String value = "10pateekes";
+        float actual = CssUtils.parseAbsoluteLength(value, "pateekes");
+        float expected = 10.0f;
+
+        Assert.assertEquals(expected, actual, 0);
+    }
 
     @Test
     public void validateMetricValue() {
