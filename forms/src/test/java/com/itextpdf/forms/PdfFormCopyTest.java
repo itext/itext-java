5b04a612e5541d864c1d8bbc4932176e39ece268
Jonathan Reilly <jonathan.reilly@itextpdf.com>
2018-09-18 09:39:02 -0400

    Add test. DEVSIX-2187

--- forms/src/test/java/com/itextpdf/forms/PdfFormCopyTest.java
+++ forms/src/test/java/com/itextpdf/forms/PdfFormCopyTest.java
@@ -192,6 +192,37 @@ public class PdfFormCopyTest extends ExtendedITextTest {
         Assert.assertNull(new CompareTool().compareByContent(destFilename, sourceFolder + "cmp_copyFields05.pdf", destinationFolder, "diff_"));
     }
 
+
+    @Test
+    @LogMessages(messages = {
+            @LogMessage(messageTemplate = LogMessageConstant.DOCUMENT_ALREADY_HAS_FIELD, count = 2)
+    })
+    public void copyMultipleSubfieldsTest01() throws IOException, InterruptedException {
+        String srcFilename = sourceFolder + "copyMultipleSubfieldsTest01.pdf";
+        String destFilename = destinationFolder + "copyMultipleSubfieldsTest01.pdf";
+
+        PdfDocument srcDoc = new PdfDocument(new PdfReader(srcFilename));
+        PdfDocument destDoc = new PdfDocument(new PdfWriter(destFilename));
+
+        PdfPageFormCopier pdfPageFormCopier = new PdfPageFormCopier();
+        // copying the same page from the same document twice
+        for (int i = 0; i < 2; ++i) {
+            srcDoc.copyPagesTo(1, 1, destDoc, pdfPageFormCopier);
+        }
+
+        PdfAcroForm acroForm = PdfAcroForm.getAcroForm(destDoc,false);
+
+        acroForm.getField("text_1").setValue("Text 1!");
+        acroForm.getField("text_2").setValue("Text 2!");
+        acroForm.getField("text.3").setValue("Text 3!");
+        acroForm.getField("text.4").setValue("Text 4!");
+
+        destDoc.close();
+        srcDoc.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destFilename, sourceFolder + "cmp_copyMultipleSubfieldsTest01.pdf", destinationFolder, "diff_"));
+    }
+
     @Test
     @LogMessages(messages = {
             @LogMessage(messageTemplate = LogMessageConstant.DOCUMENT_ALREADY_HAS_FIELD, count = 13)
@@ -357,11 +388,10 @@ public class PdfFormCopyTest extends ExtendedITextTest {
     }
 
 
-
     @Test
     public void copyPagesWithInheritedResources() throws IOException, InterruptedException {
-        String sourceFile = sourceFolder +"AnnotationSampleStandard.pdf";
-        String destFile =   destinationFolder + "AnnotationSampleStandard_copy.pdf";
+        String sourceFile = sourceFolder + "AnnotationSampleStandard.pdf";
+        String destFile = destinationFolder + "AnnotationSampleStandard_copy.pdf";
         PdfDocument source = new PdfDocument(new PdfReader(sourceFile));
         PdfDocument target = new PdfDocument(new PdfWriter(destFile));
         target.initializeOutlines();
