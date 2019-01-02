66e0d6a13d0deae4d6c358a8ea62f0807a7e29b1
iText Software <development@itextpdf.com>
2019-01-02 08:20:14 +0100

    Add missing copyright headers. Fix IXmlParser's header.

--- pdfa/src/main/java/com/itextpdf/pdfa/PdfAXMPUtil.java
+++ pdfa/src/main/java/com/itextpdf/pdfa/PdfAXMPUtil.java
@@ -1,7 +1,7 @@
 /*
  *
  * This file is part of the iText (R) project.
-    Copyright (c) 1998-2018 iText Group NV
+    Copyright (c) 1998-2019 iText Group NV
  * Authors: Bruno Lowagie, et al.
  *
  * This program is free software; you can redistribute it and/or modify
