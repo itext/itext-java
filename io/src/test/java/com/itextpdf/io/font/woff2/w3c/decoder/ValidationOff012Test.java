5a556a8b4c265ca4c14c8703665d97bd589ca806
Yauheni Borbut <yauheni.borbut@duallab.com>
2020-01-03 11:52:18 +0300

    Fix woff2 converting method
    DEVSIX-1612

--- io/src/test/java/com/itextpdf/io/font/woff2/w3c/decoder/ValidationOff012Test.java
+++ io/src/test/java/com/itextpdf/io/font/woff2/w3c/decoder/ValidationOff012Test.java
@@ -45,8 +45,6 @@ package com.itextpdf.io.font.woff2.w3c.decoder;
 import com.itextpdf.io.font.woff2.w3c.W3CWoff2DecodeTest;
 import org.junit.Ignore;
 
-@Ignore("DEVSIX-1612: different result in java and C#")
-//NOTE: Should be invalid by w3c test suite spec. See w3cProblemTest07 in html font-face test for more details.
 public class ValidationOff012Test extends W3CWoff2DecodeTest {
     @Override
     protected String getFontName() {
