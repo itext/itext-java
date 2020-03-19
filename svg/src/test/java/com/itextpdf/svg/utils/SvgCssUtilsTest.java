47675581bacab5be717748639b4c58a0a9a9b9cd
Uladzimir Asipchuk <vladimir.osipchuk@duallab.com>
2020-03-19 17:26:21 +0300

    Unignore a test on Float.MIN_VALUE.
    Now it's ported to .NET correctly.
    
    DEVSIX-2387

--- svg/src/test/java/com/itextpdf/svg/utils/SvgCssUtilsTest.java
+++ svg/src/test/java/com/itextpdf/svg/utils/SvgCssUtilsTest.java
@@ -197,7 +197,6 @@ public class SvgCssUtilsTest extends ExtendedITextTest {
         Assert.assertEquals(expected, actual);
     }
 
-    @Ignore("TODO: Check autoport for failing float comparisons. Blocked by DEVSIX-2387\n")
     @Test
     public void convertFloatMinimumToPdfTest() {
         float expected = 1.4E-45f;
