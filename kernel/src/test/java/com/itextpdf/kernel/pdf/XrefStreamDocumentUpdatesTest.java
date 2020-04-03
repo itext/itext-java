8b08b0f938c2b8501909efe8106a8cc5913a7e9f
Kate Ivanova <ekaterina.ivanova@duallab.com>
2020-04-03 14:40:33 +0300

    Add several new tests
    XrefStreamDocumentUpdatesTest#reuseIndirectRefForObjectStreamTest
    XrefStreamDocumentUpdatesTest#reuseIndRefForObjStreamInAppendModeTest
    XrefStreamDocumentUpdatesTest#freeRefReuseWhenAddNewObjTest
    
    DEVSIX-3869, DEVSIX-738

---
+++ kernel/src/test/java/com/itextpdf/kernel/pdf/XrefStreamDocumentUpdatesTest.java
@@ -0,0 +1,131 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2020 iText Group NV
+    Authors: iText Software.
+
+    This program is offered under a commercial and under the AGPL license.
+    For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+    AGPL licensing:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.itextpdf.kernel.pdf;
+
+import com.itextpdf.test.ExtendedITextTest;
+import com.itextpdf.test.annotations.type.IntegrationTest;
+
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(IntegrationTest.class)
+public class XrefStreamDocumentUpdatesTest extends ExtendedITextTest {
+    public static final String sourceFolder = "./src/test/resources/com/itextpdf/kernel/pdf/XrefStreamDocumentUpdatesTest/";
+    public static final String destinationFolder = "./target/test/com/itextpdf/kernel/pdf/XrefStreamDocumentUpdatesTest/";
+
+    @BeforeClass
+    public static void beforeClass() {
+        createDestinationFolder(destinationFolder);
+    }
+
+    @Test
+    public void readFreeRefReusingInIncrementTest() throws IOException {
+        PdfDocument document = new PdfDocument(new PdfReader
+                (sourceFolder + "readFreeRefReusingInIncrement.pdf"));
+
+        PdfArray array = (PdfArray)  document.getCatalog().getPdfObject()
+                .get(new PdfName("CustomKey"));
+
+        Assert.assertTrue(array instanceof PdfArray);
+        Assert.assertEquals(0, array.size());
+    }
+
+    @Test
+    public void notReuseIndirectRefForObjectStreamTest() throws IOException {
+        String inputFile = sourceFolder + "notReuseIndirectRefForObjectStream.pdf";
+        String outputFile = destinationFolder + "adjustingsInObjStm.pdf";
+
+        PdfDocument pdfDoc = new PdfDocument(new PdfReader(inputFile),
+                new PdfWriter(outputFile).setCompressionLevel(CompressionConstants.NO_COMPRESSION));
+
+        PdfArray media = pdfDoc.getPage(1).getPdfObject().getAsArray(PdfName.MediaBox);
+        media.remove(2);
+        media.add(new PdfNumber(500));
+        media.setModified();
+
+        pdfDoc.close();
+
+        PdfDocument doc = new PdfDocument(new PdfReader(sourceFolder + "adjustingsInObjStm.pdf"));
+        PdfObject object = doc.getPdfObject(8);
+        PdfDictionary pageDict = (PdfDictionary) object;
+
+        int expectNumberOfObjects = pdfDoc.getNumberOfPdfObjects();
+
+        //output pdf document should be openable
+        Assert.assertEquals(10, expectNumberOfObjects);
+        Assert.assertEquals(PdfName.ObjStm, pageDict.get(PdfName.Type));
+    }
+
+    @Test
+    public void notReuseIndRefForObjStreamInIncrementTest() throws IOException {
+        String inputFile = sourceFolder + "notReuseIndirectRefForObjectStream.pdf";
+        String outputFile = destinationFolder + "adjustingsInObjStmInIncrement.pdf";
+
+        PdfDocument pdfDoc = new PdfDocument(new PdfReader(inputFile),
+                new PdfWriter(outputFile).setCompressionLevel(CompressionConstants.NO_COMPRESSION),
+                new StampingProperties().useAppendMode());
+
+        PdfObject newObj = pdfDoc.getPage(1).getPdfObject();
+        newObj.setModified();
+        pdfDoc.close();
+
+        PdfDocument doc = new PdfDocument(new PdfReader(sourceFolder + "adjustingsInObjStmInIncrement.pdf"));
+
+        PdfDictionary objStmDict = (PdfDictionary) doc.getPdfObject(8);
+
+        int expectNumberOfObjects = doc.getNumberOfPdfObjects();
+
+        //output pdf document should be openable
+        Assert.assertEquals(9, expectNumberOfObjects);
+        Assert.assertEquals(PdfName.ObjStm, objStmDict.get(PdfName.Type));
+        doc.close();
+    }
+
+    @Test
+    public void freeRefReuseWhenAddNewObjTest() throws IOException {
+        String filename = destinationFolder + "freeRefReuseWhenAddNewObj.pdf";
+
+        PdfDocument pdfDoc1 = new PdfDocument(new PdfReader(sourceFolder + "pdfWithRemovedObjInOldVer.pdf"),
+                new PdfWriter(filename).setCompressionLevel(CompressionConstants.NO_COMPRESSION),
+                new StampingProperties().useAppendMode());
+        pdfDoc1.getCatalog().getPdfObject().put(new PdfName("CustomKey"), new PdfArray().makeIndirect(pdfDoc1));
+        PdfObject newObj = pdfDoc1.getCatalog().getPdfObject();
+        newObj.setModified();
+
+        int expectObjNumber = pdfDoc1.getCatalog().getPdfObject().get(new PdfName("CustomKey"))
+                .getIndirectReference().getObjNumber();
+        int expectGenNumber = pdfDoc1.getCatalog().getPdfObject().get(new PdfName("CustomKey"))
+                .getIndirectReference().getGenNumber();
+
+        PdfXrefTable xref = pdfDoc1.getXref();
+
+        Assert.assertEquals(8, expectObjNumber);
+        Assert.assertEquals(0, expectGenNumber);
+        Assert.assertTrue(xref.get(5).isFree());
+        
+        pdfDoc1.close();
+    }
+}
