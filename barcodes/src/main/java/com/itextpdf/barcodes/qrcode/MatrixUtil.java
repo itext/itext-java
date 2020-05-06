93f5697eaf56d8d7f539261cb0a37ee37c3b4f38
HaoZhong <zhonghao@sjtu.edu.cn>
2020-05-06 16:52:01 +0800

    fixing an exception error in MatrixUtil

--- barcodes/src/main/java/com/itextpdf/barcodes/qrcode/MatrixUtil.java
+++ barcodes/src/main/java/com/itextpdf/barcodes/qrcode/MatrixUtil.java
@@ -497,7 +497,7 @@ final class MatrixUtil {
     public static void makeTypeInfoBits(ErrorCorrectionLevel ecLevel, int maskPattern, BitVector bits)
             throws WriterException {
         if (!QRCode.isValidMaskPattern(maskPattern)) {
-            throw new WriterException("Invalid mask pattern");
+            throw new IllegalArgumentException("Invalid mask pattern");
         }
         int typeInfo = (ecLevel.getBits() << 3) | maskPattern;
         bits.appendBits(typeInfo, 5);
