f7a0ca8169f8f8229aeece4fdbc9e4ee8786e6b0
Kate Ivanova <ekaterina.ivanova@duallab.com>
2020-05-26 11:39:36 +0300

    Add new PdfStreamTest class, new PdfStructTreeRootTest#severalSameElementsInStructTreeRootTest
    DEVSIX-3518

--- kernel/src/test/java/com/itextpdf/kernel/pdf/PdfStreamTest.java
+++ kernel/src/test/java/com/itextpdf/kernel/pdf/PdfStreamTest.java
@@ -45,14 +45,19 @@ package com.itextpdf.kernel.pdf;
 import com.itextpdf.kernel.pdf.xobject.PdfImageXObject;
 import com.itextpdf.kernel.utils.CompareTool;
 import com.itextpdf.test.ExtendedITextTest;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import com.itextpdf.test.annotations.type.IntegrationTest;
+
+import java.util.Collections;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.rules.ExpectedException;
 
 @Category(IntegrationTest.class)
 public class PdfStreamTest extends ExtendedITextTest {
@@ -60,8 +65,11 @@ public class PdfStreamTest extends ExtendedITextTest {
     public static final String sourceFolder = "./src/test/resources/com/itextpdf/kernel/pdf/PdfStreamTest/";
     public static final String destinationFolder = "./target/test/com/itextpdf/kernel/pdf/PdfStreamTest/";
 
-    @Before
-    public void before() {
+    @Rule
+    public ExpectedException junitExpectedException = ExpectedException.none();
+
+    @BeforeClass
+    public static void before() {
         createOrClearDestinationFolder(destinationFolder);
     }
 
@@ -107,5 +115,84 @@ public class PdfStreamTest extends ExtendedITextTest {
 
         Assert.assertArrayEquals(imgBytes1, cmpImgBytes1);
         Assert.assertArrayEquals(imgBytes2, cmpImgBytes2);
+    }
+
+    @Test
+    public void indirectRefInFilterAndNoTaggedPdfTest() throws IOException {
+        String inFile = sourceFolder + "indirectRefInFilterAndNoTaggedPdf.pdf";
+        String outFile = destinationFolder + "destIndirectRefInFilterAndNoTaggedPdf.pdf";
+
+        PdfDocument srcDoc = new PdfDocument(new PdfReader(inFile));
+        PdfDocument outDoc = new PdfDocument(new PdfReader(inFile), new PdfWriter(outFile));
+        outDoc.close();
+
+        PdfDocument doc = new PdfDocument(new PdfReader(outFile));
+
+        PdfStream outStreamIm1 = doc.getFirstPage().getResources().getResource(PdfName.XObject)
+                .getAsStream(new PdfName("Im1"));
+        PdfStream outStreamIm2 = doc.getFirstPage().getResources().getResource(PdfName.XObject)
+                .getAsStream(new PdfName("Im2"));
+
+        PdfStream cmpStreamIm1 = srcDoc.getFirstPage().getResources().getResource(PdfName.XObject)
+                .getAsStream(new PdfName("Im1"));
+        PdfStream cmpStreamIm2 = srcDoc.getFirstPage().getResources().getResource(PdfName.XObject)
+                .getAsStream(new PdfName("Im2"));
+
+        Assert.assertNull(new CompareTool().compareStreamsStructure(outStreamIm1, cmpStreamIm1));
+        Assert.assertNull(new CompareTool().compareStreamsStructure(outStreamIm2, cmpStreamIm2));
+
+        srcDoc.close();
+        outDoc.close();
+    }
+
+    @Test
+    // TODO DEVSIX-1193 remove junitExpectedException and expected NullPointerException after fix
+    public void indirectFilterInCatalogTest() throws IOException {
+        String inFile = sourceFolder + "indFilterInCatalog.pdf";
+
+        PdfDocument doc = new PdfDocument(new PdfReader(inFile),
+                new PdfWriter(destinationFolder + "indFilterInCatalog.pdf"));
+
+        junitExpectedException.expect(NullPointerException.class);
+        doc.close();
+    }
+
+    @Test
+    // TODO DEVSIX-1193 remove junitExpectedException after fix
+    public void indirectFilterFlushedBeforeStreamTest() throws IOException {
+        String inFile = sourceFolder + "indFilterInCatalog.pdf";
+        String out = destinationFolder + "indirectFilterFlushedBeforeStreamTest.pdf";
+
+        PdfDocument pdfDoc = new PdfDocument(new PdfReader(inFile), new PdfWriter(out));
+
+        // Simulate the case in which filter is somehow already flushed before stream.
+        // Either directly by user or because of any other reason.
+        PdfObject filterObject = pdfDoc.getPdfObject(6);
+        filterObject.flush();
+
+        junitExpectedException.expect(NullPointerException.class);
+        pdfDoc.close();
+    }
+
+    @Test
+    // TODO DEVSIX-1193 remove junitExpectedException after fix
+    public void indirectFilterMarkedToBeFlushedBeforeStreamTest() throws IOException {
+        String inFile = sourceFolder + "indFilterInCatalog.pdf";
+        String out = destinationFolder + "indirectFilterMarkedToBeFlushedBeforeStreamTest.pdf";
+
+        PdfWriter writer = new PdfWriter(out);
+        PdfDocument pdfDoc = new PdfDocument(new PdfReader(inFile), writer);
+
+        // Simulate the case when indirect filter object is marked to be flushed before the stream itself.
+        PdfObject filterObject = pdfDoc.getPdfObject(6);
+        filterObject.getIndirectReference().setState(PdfObject.MUST_BE_FLUSHED);
+
+        // The image stream will be marked as MUST_BE_FLUSHED after page is flushed.
+        pdfDoc.getFirstPage().getPdfObject().getIndirectReference().setState(PdfObject.MUST_BE_FLUSHED);
+
+        junitExpectedException.expect(NullPointerException.class);
+        writer.flushWaitingObjects(Collections.<PdfIndirectReference>emptySet());
+
+        pdfDoc.close();
     }
 }
