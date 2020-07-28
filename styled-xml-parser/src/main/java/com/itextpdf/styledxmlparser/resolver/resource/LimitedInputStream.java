384aaf008de5f01a28989ba4fae9727542719a2b
Dmitry Chubrick <dima.chubrick@duallab.com>
2020-07-28 09:30:59 +0300

    Fix LimitedInputStream reading methods and add new unit tests
    The work of the LimitedInputStream class was brought into conformity with the work of the InputStream interface
    
    DEVSIX-3319

--- styled-xml-parser/src/main/java/com/itextpdf/styledxmlparser/resolver/resource/LimitedInputStream.java
+++ styled-xml-parser/src/main/java/com/itextpdf/styledxmlparser/resolver/resource/LimitedInputStream.java
@@ -31,8 +31,12 @@ import java.io.InputStream;
  * Implementation of the {@link InputStream} abstract class, which is used to restrict
  * reading bytes from input stream i.e. if more bytes are read than the readingByteLimit,
  * an {@link ReadingByteLimitException} exception will be thrown.
+ *
+ * Note that the readingByteLimit is not taken into account in the {@link #skip(long)},
+ * {@link #available()}, {@link #mark(int)} and {@link #reset()} methods.
  */
 class LimitedInputStream extends InputStream {
+    private boolean isStreamRead;
     private boolean isLimitViolated;
     private long readingByteLimit;
     private InputStream inputStream;
@@ -47,6 +51,7 @@ class LimitedInputStream extends InputStream {
         if (readingByteLimit < 0) {
             throw new IllegalArgumentException(StyledXmlParserExceptionMessage.READING_BYTE_LIMIT_MUST_NOT_BE_LESS_ZERO);
         }
+        this.isStreamRead = false;
         this.isLimitViolated = false;
         this.inputStream = inputStream;
         this.readingByteLimit = readingByteLimit;
@@ -54,22 +59,113 @@ class LimitedInputStream extends InputStream {
 
     @Override
     public int read() throws IOException {
+        if (isStreamRead) {
+            return -1;
+        }
         if (isLimitViolated) {
             throw new ReadingByteLimitException();
         }
 
-        readingByteLimit--;
         int nextByte = inputStream.read();
+        readingByteLimit--;
 
-        if (nextByte != -1 && readingByteLimit < 0) {
-            isLimitViolated = true;
+        checkReadingByteLimit(nextByte);
+        return nextByte;
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        if (isStreamRead) {
+            return -1;
+        }
+        if (isLimitViolated) {
             throw new ReadingByteLimitException();
         }
-        return nextByte;
+
+        int numberOfReadingBytes;
+        if (b.length > readingByteLimit) {
+            byte[] validArray;
+            if (readingByteLimit == 0) {
+                // Still need to test if end of stream is reached, so setting 1 byte to read
+                validArray = new byte[1];
+            } else {
+                // Safe to cast to int, because count is int and greater
+                validArray = new byte[(int) readingByteLimit];
+            }
+            numberOfReadingBytes = inputStream.read(validArray);
+            if (numberOfReadingBytes != -1) {
+                System.arraycopy(validArray, 0, b, 0, numberOfReadingBytes);
+            }
+        } else {
+            numberOfReadingBytes = inputStream.read(b);
+        }
+        readingByteLimit -= numberOfReadingBytes;
+
+        checkReadingByteLimit(numberOfReadingBytes);
+        return numberOfReadingBytes;
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        if (isStreamRead) {
+            return -1;
+        }
+        if (isLimitViolated) {
+            throw new ReadingByteLimitException();
+        }
+
+        if (len > readingByteLimit) {
+            if (readingByteLimit == 0) {
+                // Still need to test if end of stream is reached, so setting 1 byte to read
+                len = 1;
+            } else {
+                // Safe to cast to int, because count is int and greater
+                len = (int) readingByteLimit;
+            }
+        }
+        int numberOfReadingBytes = inputStream.read(b, off, len);
+        readingByteLimit -= numberOfReadingBytes;
+
+        checkReadingByteLimit(numberOfReadingBytes);
+        return numberOfReadingBytes;
     }
 
     @Override
     public void close() throws IOException {
         inputStream.close();
+    }
+
+    @Override
+    public long skip(long n) throws IOException {
+        return inputStream.skip(n);
+    }
+
+    @Override
+    public int available() throws IOException {
+        return inputStream.available();
+    }
+
+    @Override
+    public synchronized void mark(int readlimit) {
+        // The body of the method is empty, because markSupported method always returns false
+    }
+
+    @Override
+    public synchronized void reset() throws IOException {
+        // The body of the method is empty, because markSupported method always returns false
+    }
+
+    @Override
+    public boolean markSupported() {
+        return false;
+    }
+
+    private void checkReadingByteLimit(int byteValue) throws ReadingByteLimitException {
+        if (byteValue == -1) {
+            isStreamRead = true;
+        } else if (readingByteLimit < 0) {
+            isLimitViolated = true;
+            throw new ReadingByteLimitException();
+        }
     }
 }
--- styled-xml-parser/src/test/java/com/itextpdf/styledxmlparser/resolver/resource/LimitedInputStreamTest.java
+++ styled-xml-parser/src/test/java/com/itextpdf/styledxmlparser/resolver/resource/LimitedInputStreamTest.java
@@ -27,6 +27,7 @@ import com.itextpdf.test.ExtendedITextTest;
 import com.itextpdf.test.annotations.type.UnitTest;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -46,7 +47,7 @@ public class LimitedInputStreamTest extends ExtendedITextTest {
     public ExpectedException junitExpectedException = ExpectedException.none();
 
     @Test
-    public void readingByteLimitTest() throws IOException {
+    public void readingByteAfterFileReadingTest() throws IOException {
         UriResolver uriResolver = new UriResolver(baseUri);
         URL url = uriResolver.resolveAgainstBaseUri("retrieveStyleSheetTest.css.dat");
         // retrieveStyleSheetTest.css.dat size is 89 bytes
@@ -59,28 +60,177 @@ public class LimitedInputStreamTest extends ExtendedITextTest {
     }
 
     @Test
+    public void readingByteArrayAfterFileReadingTest() throws IOException {
+        UriResolver uriResolver = new UriResolver(baseUri);
+        URL url = uriResolver.resolveAgainstBaseUri("retrieveStyleSheetTest.css.dat");
+        // retrieveStyleSheetTest.css.dat size is 89 bytes
+        InputStream stream = new LimitedInputStream(url.openStream(), 100);
+        // The user can call the reading methods as many times as he want, and if the
+        // stream has been read, then should not throw an ReadingByteLimitException exception
+        stream.read(new byte[100]);
+        stream.read(new byte[1]);
+    }
+
+    @Test
+    public void readingByteArrayWithOffsetAfterFileReadingTest() throws IOException {
+        UriResolver uriResolver = new UriResolver(baseUri);
+        URL url = uriResolver.resolveAgainstBaseUri("retrieveStyleSheetTest.css.dat");
+        // retrieveStyleSheetTest.css.dat size is 89 bytes
+        InputStream stream = new LimitedInputStream(url.openStream(), 100);
+        // The user can call the reading methods as many times as he want, and if the
+        // stream has been read, then should not throw an ReadingByteLimitException exception
+        stream.read(new byte[100], 0, 100);
+        stream.read(new byte[1], 0, 1);
+    }
+
+    @Test
+    public void readingByteWithLimitOfOneLessThenFileSizeTest() throws IOException {
+        UriResolver uriResolver = new UriResolver(baseUri);
+        URL url = uriResolver.resolveAgainstBaseUri("retrieveStyleSheetTest.css.dat");
+        // retrieveStyleSheetTest.css.dat size is 89 bytes
+        InputStream stream = new LimitedInputStream(url.openStream(), 88);
+        for (int i = 0; i < 88; i++) {
+            Assert.assertNotEquals(-1, stream.read());
+        }
+        junitExpectedException.expect(ReadingByteLimitException.class);
+        stream.read();
+    }
+
+    @Test
     public void readingByteArrayWithLimitOfOneLessThenFileSizeTest() throws IOException {
         UriResolver uriResolver = new UriResolver(baseUri);
         URL url = uriResolver.resolveAgainstBaseUri("retrieveStyleSheetTest.css.dat");
         // retrieveStyleSheetTest.css.dat size is 89 bytes
         InputStream stream = new LimitedInputStream(url.openStream(), 88);
         byte[] bytes = new byte[100];
-        // The first time ReadingByteLimitException will be thrown, but we catch it in InputStream#read(byte[])
-        // and return 88 bytes, the second time, in the LimitedInputStream#read() method we will throw
-        // ReadingByteLimitException anyway, because readingByteLimit was violated.
         int numOfReadBytes = stream.read(bytes);
         Assert.assertEquals(88, numOfReadBytes);
         Assert.assertEquals(10, bytes[87]);
         Assert.assertEquals(0, bytes[88]);
         junitExpectedException.expect(ReadingByteLimitException.class);
+        stream.read(new byte[1]);
+    }
+
+    @Test
+    public void readingByteArrayWithOffsetAndLimitOfOneLessThenFileSizeTest() throws IOException {
+        UriResolver uriResolver = new UriResolver(baseUri);
+        URL url = uriResolver.resolveAgainstBaseUri("retrieveStyleSheetTest.css.dat");
+        // retrieveStyleSheetTest.css.dat size is 89 bytes
+        InputStream stream = new LimitedInputStream(url.openStream(), 88);
+        byte[] bytes = new byte[100];
+        int numOfReadBytes = stream.read(bytes, 0, 88);
+        Assert.assertEquals(88, numOfReadBytes);
+        Assert.assertEquals(10, bytes[87]);
+        Assert.assertEquals(0, bytes[88]);
+        junitExpectedException.expect(ReadingByteLimitException.class);
+        stream.read(bytes, 88, 1);
+    }
+
+    @Test
+    public void readingByteArrayWithSmallBufferTest() throws IOException {
+        UriResolver uriResolver = new UriResolver(baseUri);
+        URL url = uriResolver.resolveAgainstBaseUri("retrieveStyleSheetTest.css.dat");
+        // retrieveStyleSheetTest.css.dat size is 89 bytes
+        InputStream stream = new LimitedInputStream(url.openStream(), 89);
+        byte[] bytes = new byte[20];
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        while (true) {
+            int read = stream.read(bytes);
+            if (read < 1) {
+                break;
+            }
+            output.write(bytes, 0, read);
+        }
+        Assert.assertEquals(89, output.size());
+        output.close();
+    }
+
+    @Test
+    public void readingByteArrayWithBigBufferTest() throws IOException {
+        UriResolver uriResolver = new UriResolver(baseUri);
+        URL url = uriResolver.resolveAgainstBaseUri("retrieveStyleSheetTest.css.dat");
+        // retrieveStyleSheetTest.css.dat size is 89 bytes
+        InputStream stream = new LimitedInputStream(url.openStream(), 89);
+        byte[] bytes = new byte[100];
+
+        Assert.assertEquals(89, stream.read(bytes));
+        byte[] tempBytes = (byte[]) bytes.clone();
+        Assert.assertEquals(-1, stream.read(bytes));
+        // Check that the array has not changed when we have read the entire LimitedInputStream
+        Assert.assertArrayEquals(tempBytes, bytes);
+    }
+
+    @Test
+    public void readingByteArrayWithOffsetAndBigBufferTest() throws IOException {
+        UriResolver uriResolver = new UriResolver(baseUri);
+        URL url = uriResolver.resolveAgainstBaseUri("retrieveStyleSheetTest.css.dat");
+        // retrieveStyleSheetTest.css.dat size is 89 bytes
+        InputStream stream = new LimitedInputStream(url.openStream(), 89);
+        byte[] bytes = new byte[100];
+
+        Assert.assertEquals(89, stream.read(bytes, 0, 100));
+        byte[] tempBytes = (byte[]) bytes.clone();
+        Assert.assertEquals(-1, stream.read(bytes, 0, 100));
+        // Check that the array has not changed when we have read the entire LimitedInputStream
+        Assert.assertArrayEquals(tempBytes, bytes);
+    }
+
+    @Test
+    public void byteArrayOverwritingTest() throws IOException {
+        UriResolver uriResolver = new UriResolver(baseUri);
+        URL url = uriResolver.resolveAgainstBaseUri("retrieveStyleSheetTest.css.dat");
+        // retrieveStyleSheetTest.css.dat size is 89 bytes
+        InputStream stream = new LimitedInputStream(url.openStream(), 90);
+        byte[] bytes = new byte[100];
+        bytes[89] = 13;
+
+        Assert.assertEquals(89, stream.read(bytes));
+        // Check that when calling the read(byte[]) method, as many bytes were copied into
+        // the original array as were read, and not all bytes from the auxiliary array.
+        Assert.assertEquals(13, bytes[89]);
+    }
+
+    @Test
+    public void readingByteWithZeroLimitTest() throws IOException {
+        LimitedInputStream stream = new LimitedInputStream(new ByteArrayInputStream(new byte[1]), 0);
+        junitExpectedException.expect(ReadingByteLimitException.class);
+        stream.read();
+    }
+
+    @Test
+    public void readingByteArrayWithZeroLimitTest() throws IOException {
+        LimitedInputStream stream = new LimitedInputStream(new ByteArrayInputStream(new byte[1]), 0);
+        byte[] bytes = new byte[100];
+        junitExpectedException.expect(ReadingByteLimitException.class);
         stream.read(bytes);
     }
 
     @Test
-    public void zeroBytesLimitTest() throws IOException {
+    public void readingByteArrayWithOffsetAndZeroLimitTest() throws IOException {
         LimitedInputStream stream = new LimitedInputStream(new ByteArrayInputStream(new byte[1]), 0);
+        byte[] bytes = new byte[100];
         junitExpectedException.expect(ReadingByteLimitException.class);
-        stream.read();
+        stream.read(bytes, 0, 100);
+    }
+
+    @Test
+    public void readingEmptyByteWithZeroLimitTest() throws IOException {
+        LimitedInputStream stream = new LimitedInputStream(new ByteArrayInputStream(new byte[0]), 0);
+        Assert.assertEquals(-1, stream.read());
+    }
+
+    @Test
+    public void readingEmptyByteArrayWithZeroLimitTest() throws IOException {
+        LimitedInputStream stream = new LimitedInputStream(new ByteArrayInputStream(new byte[0]), 0);
+        byte[] bytes = new byte[100];
+        Assert.assertEquals(-1, stream.read(bytes));
+    }
+
+    @Test
+    public void readingEmptyByteArrayWithOffsetAndZeroLimitTest() throws IOException {
+        LimitedInputStream stream = new LimitedInputStream(new ByteArrayInputStream(new byte[0]), 0);
+        byte[] bytes = new byte[100];
+        Assert.assertEquals(-1, stream.read(bytes, 0, 100));
     }
 
     @Test
