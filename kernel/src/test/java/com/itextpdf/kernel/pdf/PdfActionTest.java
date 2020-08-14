46b5410b81555976d1d9f73cae354db89a27aa1a
Artyom Yanchevsky <artyom.yanchevsky@duallab.com>
2020-08-14 12:01:33 +0300

    Add tests for PdfAction
    DEVSIX-4357

--- kernel/src/test/java/com/itextpdf/kernel/pdf/PdfActionTest.java
+++ kernel/src/test/java/com/itextpdf/kernel/pdf/PdfActionTest.java
@@ -42,12 +42,24 @@
  */
 package com.itextpdf.kernel.pdf;
 
+import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.action.PdfAction;
+import com.itextpdf.kernel.pdf.action.PdfActionOcgState;
+import com.itextpdf.kernel.pdf.annot.PdfAnnotation;
+import com.itextpdf.kernel.pdf.annot.PdfLineAnnotation;
+import com.itextpdf.kernel.pdf.filespec.PdfStringFS;
+import com.itextpdf.kernel.utils.CompareTool;
 import com.itextpdf.test.ExtendedITextTest;
 import com.itextpdf.test.annotations.type.IntegrationTest;
 
 import com.itextpdf.io.util.MessageFormatUtil;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -83,7 +95,262 @@ public class PdfActionTest extends ExtendedITextTest {
         document.close();
 
         System.out.println(MessageFormatUtil.format("Please open document {0} at page 2 and make sure that you're automatically redirected to {1} site.", destinationFolder + "actionTest02.pdf", "http://itextpdf.com"));
+    }
 
+    @Test
+    public void soundActionTest() throws Exception {
+        String fileName = "soundActionTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        InputStream is = new FileInputStream(sourceFolder + "sample.aif");
+        PdfStream sound1 = new PdfStream(document, is);
+        sound1.put(PdfName.R, new PdfNumber(32117));
+        sound1.put(PdfName.E, PdfName.Signed);
+        sound1.put(PdfName.B, new PdfNumber(16));
+        sound1.put(PdfName.C, new PdfNumber(1));
+
+        document.getPage(2).setAdditionalAction(PdfName.O, PdfAction.createSound(sound1));
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void soundActionWithRepeatFlagTest() throws Exception {
+        String fileName = "soundActionWithRepeatFlagTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        InputStream is = new FileInputStream(sourceFolder + "sample.aif");
+        PdfStream sound1 = new PdfStream(document, is);
+        sound1.put(PdfName.R, new PdfNumber(32117));
+        sound1.put(PdfName.E, PdfName.Signed);
+        sound1.put(PdfName.B, new PdfNumber(16));
+        sound1.put(PdfName.C, new PdfNumber(1));
+
+        document.getPage(2)
+                .setAdditionalAction(PdfName.O, PdfAction.createSound(sound1,1f, false, true, false));
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void soundActionWithToBigVolumeTest() throws Exception {
+        PdfDocument document = createDocument(new PdfWriter(new ByteArrayOutputStream()), false);
+
+        InputStream is = new FileInputStream(sourceFolder + "sample.aif");
+        PdfStream sound1 = new PdfStream(document, is);
+        sound1.put(PdfName.R, new PdfNumber(32117));
+        sound1.put(PdfName.E, PdfName.Signed);
+        sound1.put(PdfName.B, new PdfNumber(16));
+        sound1.put(PdfName.C, new PdfNumber(1));
+
+        try {
+            document.getPage(2)
+                    .setAdditionalAction(PdfName.O, PdfAction.createSound(sound1, 1.1f, false, false, false));
+            Assert.fail("Exception not thrown");
+        } catch (Exception e) {
+            Assert.assertEquals("volume", e.getMessage());
+        }
+        document.close();
+    }
+
+    @Test
+    public void soundActionWithToLowVolumeTest() throws Exception {
+        PdfDocument document = createDocument(new PdfWriter(new ByteArrayOutputStream()), false);
+
+        InputStream is = new FileInputStream(sourceFolder + "sample.aif");
+        PdfStream sound1 = new PdfStream(document, is);
+        sound1.put(PdfName.R, new PdfNumber(32117));
+        sound1.put(PdfName.E, PdfName.Signed);
+        sound1.put(PdfName.B, new PdfNumber(16));
+        sound1.put(PdfName.C, new PdfNumber(1));
+
+        try {
+            document.getPage(2)
+                    .setAdditionalAction(PdfName.O, PdfAction.createSound(sound1, -1.1f, false, false, false));
+            Assert.fail("Exception not thrown");
+        } catch (Exception e) {
+            Assert.assertEquals("volume", e.getMessage());
+        }
+        document.close();
+    }
+
+    @Test
+    public void ocgStateTest() throws Exception {
+        PdfName stateName = PdfName.ON;
+
+        PdfDictionary ocgDict1 = new PdfDictionary();
+        ocgDict1.put(PdfName.Type, PdfName.OCG);
+        ocgDict1.put(PdfName.Name, new PdfName("ocg1"));
+
+        PdfDictionary ocgDict2 = new PdfDictionary();
+        ocgDict2.put(PdfName.Type, PdfName.OCG);
+        ocgDict2.put(PdfName.Name, new PdfName("ocg2"));
+
+        List<PdfDictionary> dicts = new ArrayList<>();
+        dicts.add(ocgDict1);
+        dicts.add(ocgDict2);
+
+        List<PdfActionOcgState> ocgStates = new ArrayList<>();
+        ocgStates.add(new PdfActionOcgState(stateName, dicts));
+
+        String fileName = "ocgStateTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+        document.getPage(1).setAdditionalAction(PdfName.O, PdfAction.createSetOcgState(ocgStates));
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void launchActionTest() throws Exception {
+        String fileName = "launchActionTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        document.getPage(1).setAdditionalAction(PdfName.O, PdfAction.createLaunch(new PdfStringFS("launch.sh")));
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void launchActionOnNewWindowTest() throws Exception {
+        String fileName = "launchActionOnNewWindowTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        document.getPage(1).setAdditionalAction(PdfName.O,
+                PdfAction.createLaunch(new PdfStringFS("launch.sh"), true));
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void createHiddenAnnotationTest() throws Exception {
+        String fileName = "createHiddenAnnotationTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        PdfAnnotation annotation = new PdfLineAnnotation(new Rectangle(10, 10, 200, 200),
+                new float[] {50, 750, 50, 750});
+        document.getPage(1).setAdditionalAction(PdfName.O, PdfAction.createHide(annotation, true));
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void createHiddenAnnotationsTest() throws Exception {
+        String fileName = "createHiddenAnnotationsTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        PdfAnnotation[] annotations = new PdfAnnotation[] {
+                new PdfLineAnnotation(new Rectangle(10, 10, 200, 200), new float[] {50, 750, 50, 750}),
+                new PdfLineAnnotation(new Rectangle(200, 200, 200, 200), new float[] {50, 750, 50, 750})
+        };
+        document.getPage(1).setAdditionalAction(PdfName.O, PdfAction.createHide(annotations, true));
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void createHiddenByFieldNameTest() throws Exception {
+        String fileName = "createHiddenByFieldNameTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        document.getPage(1).setAdditionalAction(PdfName.O, PdfAction.createHide("name", true));
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void createHiddenByFieldNamesTest() throws Exception {
+        String fileName = "createHiddenByFieldNamesTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        document.getPage(1).setAdditionalAction(PdfName.O, PdfAction.createHide(new String[] {"name1", "name2"}, true));
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void createNamedTest() throws Exception {
+        String fileName = "createNamedTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        document.getPage(1).setAdditionalAction(PdfName.O, PdfAction.createNamed(PdfName.LastPage));
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void createJavaScriptTest() throws Exception {
+        String fileName = "createJavaScriptTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        String javaScriptRotatePages = "this.setPageRotations(0,2,90)";
+        document.getPage(1).setAdditionalAction(PdfName.O, PdfAction.createJavaScript(javaScriptRotatePages));
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void soundAndNextJavaScriptActionTest() throws Exception {
+        String fileName = "soundAndNextJavaScriptActionTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        InputStream is = new FileInputStream(sourceFolder + "sample.aif");
+        PdfStream sound1 = new PdfStream(document, is);
+        sound1.put(PdfName.R, new PdfNumber(32117));
+        sound1.put(PdfName.E, PdfName.Signed);
+        sound1.put(PdfName.B, new PdfNumber(16));
+        sound1.put(PdfName.C, new PdfNumber(1));
+
+        PdfAction action = PdfAction.createSound(sound1);
+        action.next(PdfAction.createJavaScript("this.setPageRotations(0,2,90)"));
+        document.getPage(2).setAdditionalAction(PdfName.O, action);
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
+    }
+
+    @Test
+    public void soundAndTwoNextJavaScriptActionTest() throws Exception {
+        String fileName = "soundAndTwoNextJavaScriptActionTest.pdf";
+        PdfDocument document = createDocument(new PdfWriter(destinationFolder + fileName), false);
+
+        InputStream is = new FileInputStream(sourceFolder + "sample.aif");
+        PdfStream sound1 = new PdfStream(document, is);
+        sound1.put(PdfName.R, new PdfNumber(32117));
+        sound1.put(PdfName.E, PdfName.Signed);
+        sound1.put(PdfName.B, new PdfNumber(16));
+        sound1.put(PdfName.C, new PdfNumber(1));
+
+        PdfAction action = PdfAction.createSound(sound1);
+        action.next(PdfAction.createJavaScript("this.setPageRotations(0,2,90)"));
+        action.next(PdfAction.createJavaScript("this.setPageRotations(0,2,180)"));
+        document.getPage(2).setAdditionalAction(PdfName.O, action);
+        document.close();
+
+        Assert.assertNull(new CompareTool().compareByContent(destinationFolder + fileName,
+                sourceFolder + "cmp_" + fileName, destinationFolder, "diff_"));
     }
 
     private PdfDocument createDocument(PdfWriter writer, boolean flushPages) {
