86e2d518cf3bf4ca60842b4e1084a7f57db35fba
Zuzu-Typ <Zuzu.Typ@gmail.com>
2020-08-31 10:01:49 +0200

    Fixed documentation
    DEVSIX-8737

--- kernel/src/main/java/com/itextpdf/kernel/pdf/EncryptionConstants.java
+++ kernel/src/main/java/com/itextpdf/kernel/pdf/EncryptionConstants.java
@@ -55,39 +55,39 @@ public final class EncryptionConstants {
      */
     public static final int DO_NOT_ENCRYPT_METADATA = 8;
     /**
-     * Add this to the mode to keep encrypt only the embedded files.
+     * Add this to the mode to encrypt only the embedded files.
      */
     public static final int EMBEDDED_FILES_ONLY = 24;
     /**
-     * The operation permitted when the document is opened with the user password.
+     * The operation is permitted when the document is opened with the user password.
      */
     public static final int ALLOW_PRINTING = 4 + 2048;
     /**
-     * The operation permitted when the document is opened with the user password.
+     * The operation is permitted when the document is opened with the user password.
      */
     public static final int ALLOW_MODIFY_CONTENTS = 8;
     /**
-     * The operation permitted when the document is opened with the user password.
+     * The operation is permitted when the document is opened with the user password.
      */
     public static final int ALLOW_COPY = 16;
     /**
-     * The operation permitted when the document is opened with the user password.
+     * The operation is permitted when the document is opened with the user password.
      */
     public static final int ALLOW_MODIFY_ANNOTATIONS = 32;
     /**
-     * The operation permitted when the document is opened with the user password.
+     * The operation is permitted when the document is opened with the user password.
      */
     public static final int ALLOW_FILL_IN = 256;
     /**
-     * The operation permitted when the document is opened with the user password.
+     * The operation is permitted when the document is opened with the user password.
      */
     public static final int ALLOW_SCREENREADERS = 512;
     /**
-     * The operation permitted when the document is opened with the user password.
+     * The operation is permitted when the document is opened with the user password.
      */
     public static final int ALLOW_ASSEMBLY = 1024;
     /**
-     * The operation permitted when the document is opened with the user password.
+     * The operation is permitted when the document is opened with the user password.
      */
     public static final int ALLOW_DEGRADED_PRINTING = 4;
 
