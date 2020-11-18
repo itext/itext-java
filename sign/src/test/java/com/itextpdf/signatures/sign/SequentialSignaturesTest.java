f810573aa4128c64bbead6982f1c1486cf572a73
Kate Ivanova <ekaterina.ivanova@duallab.com>
2020-11-18 19:20:50 +0300

    Add test on second time signing document
    DEVSIX-4769

--- sign/src/test/java/com/itextpdf/signatures/sign/SequentialSignaturesTest.java
+++ sign/src/test/java/com/itextpdf/signatures/sign/SequentialSignaturesTest.java
@@ -43,16 +43,22 @@
 package com.itextpdf.signatures.sign;
 
 import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.CompressionConstants;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfName;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.StampingProperties;
 import com.itextpdf.signatures.BouncyCastleDigest;
 import com.itextpdf.signatures.DigestAlgorithms;
 import com.itextpdf.signatures.IExternalSignature;
+import com.itextpdf.signatures.PdfSignatureAppearance;
 import com.itextpdf.signatures.PdfSigner;
+import com.itextpdf.signatures.PdfSigner.CryptoStandard;
 import com.itextpdf.signatures.PrivateKeySignature;
 import com.itextpdf.test.signutils.Pkcs12FileHelper;
 import com.itextpdf.test.ExtendedITextTest;
 import com.itextpdf.test.annotations.type.IntegrationTest;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -60,6 +66,7 @@ import java.security.PrivateKey;
 import java.security.Security;
 import java.security.cert.Certificate;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -100,5 +107,57 @@ public class SequentialSignaturesTest extends ExtendedITextTest {
         signer.signDetached(new BouncyCastleDigest(), pks, signChain, null, null, null, 0, PdfSigner.CryptoStandard.CADES);
 
         PadesSigTest.basicCheckSignedDoc(outFileName, signatureName);
+    }
+
+    @Test
+    public void secondSignOfTaggedDocTest() throws IOException, GeneralSecurityException {
+        String signCertFileName = certsSrc + "signCertRsa01.p12";
+        String outFileName = destinationFolder + "secondSignOfTagged.pdf";
+        String srcFileName = sourceFolder + "taggedAndSignedDoc.pdf";
+
+        Certificate[] signChain = Pkcs12FileHelper.readFirstChain(signCertFileName, password);
+
+        PrivateKey signPrivateKey = Pkcs12FileHelper.readFirstKey(signCertFileName, password, password);
+
+        IExternalSignature pks = new PrivateKeySignature(signPrivateKey, DigestAlgorithms.SHA256,
+                BouncyCastleProvider.PROVIDER_NAME);
+
+        String signatureName = "Signature2";
+        PdfSigner signer = new PdfSigner(new PdfReader(srcFileName), new FileOutputStream(outFileName),
+                new StampingProperties().useAppendMode());
+
+        PdfDocument document = signer.getDocument();
+        document.getWriter().setCompressionLevel(CompressionConstants.NO_COMPRESSION);
+
+        signer.setFieldName(signatureName);
+        PdfSignatureAppearance appearance = signer.getSignatureAppearance();
+        appearance.setPageNumber(1);
+        signer.getSignatureAppearance()
+                .setPageRect(new Rectangle(50, 550, 200, 100))
+                .setReason("Test2")
+                .setLocation("TestCity2")
+                .setLayer2Text("Approval test signature #2.\nCreated by iText7.");
+
+        signer.signDetached(new BouncyCastleDigest(), pks, signChain, null, null,
+                null, 0, CryptoStandard.CADES);
+
+        PadesSigTest.basicCheckSignedDoc(outFileName, "Signature1");
+        PadesSigTest.basicCheckSignedDoc(outFileName, "Signature2");
+
+        try (PdfDocument twiceSigned = new PdfDocument(new PdfReader(outFileName));
+             PdfDocument resource = new PdfDocument(new PdfReader(srcFileName))) {
+
+            float resourceStrElemNumber = resource.getStructTreeRoot().getPdfObject().getAsArray(PdfName.K)
+                    .getAsDictionary(0).getAsArray(PdfName.K).size();
+
+            float outStrElemNumber = twiceSigned.getStructTreeRoot().getPdfObject().getAsArray(PdfName.K)
+                    .getAsDictionary(0).getAsArray(PdfName.K).size();
+
+            // Here we assert the amount of objects in StructTreeRoot in resource file and twice signed file
+            // as the original signature validation failed by Adobe because of struct tree change. If the fix
+            // would make this tree unchanged, then the assertion should be adjusted with comparing the tree of
+            // objects in StructTreeRoot to ensure that it won't be changed.
+            Assert.assertNotEquals(resourceStrElemNumber, outStrElemNumber);
+        }
     }
 }
