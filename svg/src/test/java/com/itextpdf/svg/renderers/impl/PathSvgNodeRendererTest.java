fd681f7b4221d0ca4d131d50335ab0b2e5971faf
Kate Ivanova <ekaterina.ivanova@duallab.com>
2020-12-08 16:37:40 +0300

    Add tests for SVG moveTo followed by multiple pairs
    DEVSIX-4846

--- svg/src/test/java/com/itextpdf/svg/renderers/impl/PathSvgNodeRendererTest.java
+++ svg/src/test/java/com/itextpdf/svg/renderers/impl/PathSvgNodeRendererTest.java
@@ -410,4 +410,27 @@ public class PathSvgNodeRendererTest extends SvgIntegrationTest {
         convertAndCompare(sourceFolder, destinationFolder, "eofillUnsuportedPathTest");
     }
 
+    @Test
+    //TODO: update cmp-file after DEVSIX-4701 will be fixed
+    public void multiplePairsAfterMoveToRelativeTest() throws IOException, InterruptedException {
+        convertAndCompare(sourceFolder, destinationFolder, "multiplePairsAfterMoveToRelative");
+    }
+
+    @Test
+    //TODO: update cmp-file after DEVSIX-4701 will be fixed
+    public void multiplePairsAfterMoveToAbsoluteTest() throws IOException, InterruptedException {
+        convertAndCompare(sourceFolder, destinationFolder, "multiplePairsAfterMoveToAbsolute");
+    }
+
+    @Test
+    //TODO: update cmp-file after DEVSIX-4701 will be fixed
+    public void pathHOperatorAbsoluteAfterMultiplePairsTest() throws IOException, InterruptedException {
+        convertAndCompare(sourceFolder, destinationFolder, "pathHOperatorAbsoluteAfterMultiplePairs");
+    }
+
+    @Test
+    //TODO: update cmp-file after DEVSIX-4701 will be fixed
+    public void pathHOperatorRelativeAfterMultiplePairsTest() throws IOException, InterruptedException {
+        convertAndCompare(sourceFolder, destinationFolder, "pathHOperatorRelativeAfterMultiplePairs");
+    }
 }
