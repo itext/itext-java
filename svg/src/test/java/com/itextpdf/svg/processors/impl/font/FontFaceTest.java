ae78654a54a538eb6e89bbf4ac8e824bbd2e7428
Egor Martsynkovsky <egor.martsynkovsky@duallab.com>
2021-08-12 13:06:12 +0300

    Link TODOs to corresponding tickets
    DEVSIX-5693

--- svg/src/test/java/com/itextpdf/svg/processors/impl/font/FontFaceTest.java
+++ svg/src/test/java/com/itextpdf/svg/processors/impl/font/FontFaceTest.java
@@ -162,7 +162,7 @@ public class FontFaceTest extends SvgIntegrationTest {
     }
 
     @Test
-    //TODO: In w3c test suite this font is labeled as invalid though it correctly parsers both in browser and iText
+    //TODO(DEVSIX-5755): In w3c test suite this font is labeled as invalid though it correctly parsers both in browser and iText
     //See BlocksMetadataPadding001Test in io for decompression details
     public void w3cProblemTest01() throws IOException, InterruptedException {
         runTest("w3cProblemTest01");
@@ -181,7 +181,7 @@ public class FontFaceTest extends SvgIntegrationTest {
     }
 
     @Test
-    //TODO: silently omitted, decompression should fail.
+    //TODO(DEVSIX-5756): silently omitted, decompression should fail.
     //See HeaderFlavor001Test in io for decompression details
     public void w3cProblemTest03() throws IOException, InterruptedException {
         runTest("w3cProblemTest03");
@@ -189,7 +189,7 @@ public class FontFaceTest extends SvgIntegrationTest {
 
     @Test
     @LogMessages(messages = {@LogMessage(messageTemplate = com.itextpdf.io.LogMessageConstant.FONT_SUBSET_ISSUE)})
-    //TODO: silently omitted, decompression should fail. Browser loads font but don't draw glyph.
+    //TODO(DEVSIX-5756): silently omitted, decompression should fail. Browser loads font but don't draw glyph.
     //See HeaderFlavor002Test in io for decompression details
     public void w3cProblemTest04() throws IOException, InterruptedException {
         //NOTE, iText fails on subsetting as expected.
@@ -197,14 +197,14 @@ public class FontFaceTest extends SvgIntegrationTest {
     }
 
     @Test
-    //TODO: In w3c test suite this font is labeled as invalid though it correctly parsers both in browser and iText
+    //TODO(DEVSIX-5755): In w3c test suite this font is labeled as invalid though it correctly parsers both in browser and iText
     //See HeaderReserved001Test in io for decompression details
     public void w3cProblemTest05() throws IOException, InterruptedException {
         runTest("w3cProblemTest05");
     }
 
     @Test
-    //TODO: In w3c test suite this font is labeled as invalid though it correctly parsers both in browser and iText
+    //TODO(DEVSIX-5755): In w3c test suite this font is labeled as invalid though it correctly parsers both in browser and iText
     //See TabledataHmtxTransform003Test in io for decompression details
     public void w3cProblemTest06() throws IOException, InterruptedException {
         runTest("w3cProblemTest06");
