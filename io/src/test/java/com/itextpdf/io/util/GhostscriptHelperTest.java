4c64bcd79da8a8a2e1fbd45a1dc6225684c89c6d
Kate Ivanova <ekaterina.ivanova@duallab.com>
2022-01-14 11:49:16 +0300

    Upgrade org.slf4j:slf4j-log4j12, ch.qos.logback:logback-classic, logback-core, bouncycastle versions
    Update bouncycastle version from 1.69 to 1.70
    Update variable for GhostscriptHelperTest in order to be runnable on more systems
    
    DEVSIX-6218, DEVSIX-6278

--- io/src/test/java/com/itextpdf/io/util/GhostscriptHelperTest.java
+++ io/src/test/java/com/itextpdf/io/util/GhostscriptHelperTest.java
@@ -64,7 +64,7 @@ public class GhostscriptHelperTest extends ExtendedITextTest {
 
     // In some of the test we will check whether Ghostscript has printed its help message to the console.
     // The value of this threshold should be definitely less than the length of the help message.
-    private static final int SYSTEM_OUT_LENGTH_LIMIT = 400;
+    private static final int SYSTEM_OUT_LENGTH_LIMIT = 450;
 
     @Before
     public void setUp() {
