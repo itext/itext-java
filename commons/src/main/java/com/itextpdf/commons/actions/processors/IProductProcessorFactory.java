43bc87cda2c08ee3ba417fa2fb80099771066211
Guust Ysebie <guust.ysebie@itextpdf.com>
2023-01-03 15:31:22 +0100

    Update copyright notice to 2023
    DEVSIX-6437

--- commons/src/main/java/com/itextpdf/commons/actions/processors/IProductProcessorFactory.java
+++ commons/src/main/java/com/itextpdf/commons/actions/processors/IProductProcessorFactory.java
@@ -1,6 +1,6 @@
 /*
     This file is part of the iText (R) project.
-    Copyright (c) 1998-2022 iText Group NV
+    Copyright (c) 1998-2023 iText Group NV
     Authors: iText Software.
 
     This program is offered under a commercial and under the AGPL license.
