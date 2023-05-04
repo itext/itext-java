773e97e31bf6451ca603d57d9919a4bff0ce5733
Dmitry Chubrick <dmitry.chubrick@apryse.com>
2023-05-04 16:20:02 +0300

    Remove bouncy-castle-fips-adapter module
    DEVSIX-7351

--- bouncy-castle-connector/src/main/java/com/itextpdf/bouncycastleconnector/logs/BouncyCastleLogMessageConstant.java
+++ bouncy-castle-connector/src/main/java/com/itextpdf/bouncycastleconnector/logs/BouncyCastleLogMessageConstant.java
@@ -32,5 +32,5 @@ public final class BouncyCastleLogMessageConstant {
     }
 
     public static final String BOUNCY_CASTLE_DEPENDENCY_MUST_PRESENT =
-            "Either com.itextpdf:bouncy-castle-adapter or com.itextpdf:bouncy-castle-fips-adapter dependency must be added in order to use BouncyCastleFactoryCreator";
+            "com.itextpdf.android:bouncy-castle-adapter-android dependency must be added in order to use BouncyCastleFactoryCreator";
 }
