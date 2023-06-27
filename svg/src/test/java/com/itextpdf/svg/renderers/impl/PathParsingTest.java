b006317fd7045a987be9df93044295e75f9ecd37
Vitali Prudnikovich <vitali.prudnikovich@apryse.com>
2023-06-27 08:58:05 +0300

    Fix exponential notation usage in svg path

--- svg/src/test/java/com/itextpdf/svg/renderers/impl/PathParsingTest.java
+++ svg/src/test/java/com/itextpdf/svg/renderers/impl/PathParsingTest.java
@@ -197,4 +197,13 @@ public class PathParsingTest extends ExtendedITextTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void exponentInNumberTest02() {
+        PathSvgNodeRenderer path = new PathSvgNodeRenderer();
+        String input = "C 268.88888888888886 67.97916666666663e+10 331.1111111111111 -2.842170943040401E-14 393.3333333333333 -2.842170943040401E-14";
+
+        String expected = "C 268.88888888888886 67.97916666666663e+10 331.1111111111111 -2.842170943040401E-14 393.3333333333333 -2.842170943040401E-14";
+        String actual = path.separateDecimalPoints(input);
+        Assert.assertEquals(expected, actual);
+    }
 }
