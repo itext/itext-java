a11324c23c8ca8807b07f536579091b8b3c486f0
Dmitry Chubrick <dmitry.chubrick@apryse.com>
2023-11-27 15:36:00 +0300

    Linked 2 TODOs from ImagePdfBytesInfo
    DEVSIX-7907

--- kernel/src/main/java/com/itextpdf/kernel/pdf/xobject/ImagePdfBytesInfo.java
+++ kernel/src/main/java/com/itextpdf/kernel/pdf/xobject/ImagePdfBytesInfo.java
@@ -141,8 +141,8 @@ class ImagePdfBytesInfo {
         byte[] newImageBytes = scs.getTintTransformation().calculateFromByteArray(imageBytes, 0,
                 imageBytes.length, 8, 8
         );
-        // TODO switch top tiff for CMYK
-        // TODO verify RGBA is working
+        // TODO DEVSIX-6757 switch top tiff for CMYK
+        // TODO DEVSIX-6757 verify RGBA is working
         if (scs.getBaseCs().getNumberOfComponents() > 3) {
             throw new UnsupportedOperationException(KernelExceptionMessageConstant.
                     GET_IMAGEBYTES_FOR_SEPARATION_COLOR_ONLY_SUPPORTS_RGB);
