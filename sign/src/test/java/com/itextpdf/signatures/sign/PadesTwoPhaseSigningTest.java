4dcad825a0685a8a9b9ae0a6346310de34754ac5
iText Software <development@itextpdf.com>
2024-01-04 02:11:18 +0000

    Add missing copyright headers

--- sign/src/test/java/com/itextpdf/signatures/sign/PadesTwoPhaseSigningTest.java
+++ sign/src/test/java/com/itextpdf/signatures/sign/PadesTwoPhaseSigningTest.java
@@ -1,3 +1,25 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2024 Apryse Group NV
+    Authors: Apryse Software.
+
+    This program is offered under a commercial and under the AGPL license.
+    For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+    AGPL licensing:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.itextpdf.signatures.sign;
 
 import com.itextpdf.bouncycastleconnector.BouncyCastleFactoryCreator;
@@ -49,7 +71,7 @@ public class PadesTwoPhaseSigningTest extends ExtendedITextTest {
 
     @Test
     public void differentDigestAlgorithmsTest() throws Exception {
-        String fileName = "twoStepSigningBaselineLTATest.pdf";
+        String fileName = "differentDigestAlgorithmsTest.pdf";
         String outFileName = destinationFolder + fileName;
         String srcFileName = sourceFolder + "helloWorldDoc.pdf";
         String signCertFileName = certsSrc + "signCertRsa01.pem";
@@ -91,7 +113,7 @@ public class PadesTwoPhaseSigningTest extends ExtendedITextTest {
 
     @Test
     public void missingTimestampClientTest() throws Exception {
-        String fileName = "twoStepSigningBaselineLTATest.pdf";
+        String fileName = "missingTimestampClientTest.pdf";
         String outFileName = destinationFolder + fileName;
         String srcFileName = sourceFolder + "helloWorldDoc.pdf";
         String signCertFileName = certsSrc + "signCertRsa01.pem";
