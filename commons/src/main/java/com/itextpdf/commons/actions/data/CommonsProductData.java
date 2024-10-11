539cff6ef17d323d2ec3e699f4c93f82dadef6b6
Apryse Software <development@apryse.com>
2024-10-11 13:11:52 +0000

    [AFTER RELEASE] 9.0.0

--- commons/src/main/java/com/itextpdf/commons/actions/data/CommonsProductData.java
+++ commons/src/main/java/com/itextpdf/commons/actions/data/CommonsProductData.java
@@ -28,7 +28,7 @@ package com.itextpdf.commons.actions.data;
 public final class CommonsProductData {
     static final String COMMONS_PUBLIC_PRODUCT_NAME = "Commons";
     static final String COMMONS_PRODUCT_NAME = "commons";
-    static final String COMMONS_VERSION = "9.0.0-SNAPSHOT";
+    static final String COMMONS_VERSION = "9.0.1-SNAPSHOT";
     static final String MINIMAL_COMPATIBLE_LICENSEKEY_VERSION = "4.1.0";
     static final int COMMONS_COPYRIGHT_SINCE = 2000;
     static final int COMMONS_COPYRIGHT_TO = 2024;
