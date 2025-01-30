e6fb263226c008ce09a93f34771c2389d6c36fee
Vitali Prudnikovich <vitali.prudnikovich@apryse.com>
2025-01-30 11:17:18 +0300

    Fix annotation UA-1 checks
    DEVSIX-8679

--- pdfua/src/main/java/com/itextpdf/pdfua/checkers/utils/AnnotationCheckUtil.java
+++ pdfua/src/main/java/com/itextpdf/pdfua/checkers/utils/AnnotationCheckUtil.java
@@ -126,8 +126,9 @@ public final class AnnotationCheckUtil {
                 throw new PdfUAConformanceException(PdfUAExceptionMessageConstants.ANNOT_TRAP_NET_IS_NOT_PERMITTED);
             }
 
-            if (!PdfName.Widget.equals(subtype) && !(annotObj.containsKey(PdfName.Contents)
-                    || annotObj.containsKey(PdfName.Alt))) {
+            PdfStructElem parent = (PdfStructElem) objRef.getParent();
+            if (!PdfName.Widget.equals(subtype) &&
+                    !(annotObj.containsKey(PdfName.Contents) || (parent != null && parent.getAlt() != null))) {
                 throw new PdfUAConformanceException(MessageFormatUtil.format(
                         PdfUAExceptionMessageConstants.ANNOTATION_OF_TYPE_0_SHOULD_HAVE_CONTENTS_OR_ALT_KEY, subtype.getValue()));
             }
