6bcb71019faaa802f29f0e4c19bd8bfcaae22d12
Dmitry Chubrick <dmitry.chubrick@apryse.com>
2025-03-18 12:21:26 +0300

    Fix jsoup copyright attributions in iText Core and pdfHTML
    DEVSIX-8999

--- styled-xml-parser/src/test/java/com/itextpdf/styledxmlparser/jsoup/helper/ValidateTest.java
+++ styled-xml-parser/src/test/java/com/itextpdf/styledxmlparser/jsoup/helper/ValidateTest.java
@@ -1,25 +1,7 @@
 /*
-    This file is part of the iText (R) project.
-    Copyright (c) 1998-2025 Apryse Group NV
-    Authors: Apryse Software.
-
-    This program is offered under a commercial and under the AGPL license.
-    For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
-
-    AGPL licensing:
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+    This file is part of jsoup, see NOTICE.txt in the root of the repository.
+    It may contain modifications beyond the original version.
+*/
 package com.itextpdf.styledxmlparser.jsoup.helper;
 
 import com.itextpdf.test.ExtendedITextTest;
